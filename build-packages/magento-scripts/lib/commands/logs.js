cf57cb655dd9029226b064d3b86bdbaeec665424
ejnshtein <ejnshtein@gmail.com>
2020-11-23 13:33:22 +0200

    add logs command

--- build-packages/magento-scripts/lib/commands/logs.js
+++ build-packages/magento-scripts/lib/commands/logs.js
@@ -1,5 +1,29 @@
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
+const { docker } = require('../config');
+const { execAsyncSpawn } = require('../util/exec-async-command');
+
 module.exports = (yargs) => {
-    yargs.command('logs', 'Display application logs.', () => {}, (argv) => {
-        console.log('m2 lgs', argv);
+    yargs.command('logs <scope>', 'Display application logs.', () => {}, async (argv) => {
+        const containers = docker.getContainers();
+        const services = Object.keys(containers);
+
+        if (services.includes(argv.scope)) {
+            const { result } = await execAsyncSpawn(`docker logs ${containers[argv.scope].name}`, {
+                withCode: true
+            });
+
+            logger.log(result);
+            return;
+        }
+
+        if (argv.scope === 'magento') {
+            await execAsyncSpawn('tail -f var/log/system.log', {
+                callback: logger.log
+            });
+
+            return;
+        }
+
+        logger.error(`No service found "${argv.scope}"`);
     });
 };
