c6995539114630e240cc05bc38b2716ce7dc1b6a
ejnshtein <ejnshtein@gmail.com>
2020-11-24 13:20:38 +0200

    platform error message fix

--- build-packages/magento-scripts/lib/tasks/requirements/platform.js
+++ build-packages/magento-scripts/lib/tasks/requirements/platform.js
@@ -10,7 +10,7 @@ const checkPlatform = {
         if (!platforms.includes(currentPlatform)) {
             throw new Error(
                 `Your current OS platform is ${ logger.style.misc(currentPlatform) }.
-                Unfortunately, currently we only support ${ platforms.map((platform) => logger.style.misc(platform).join(',')) }.`
+                Unfortunately, currently we only support ${ platforms.map((platform) => logger.style.misc(platform)).join(',') }.`
             );
         }
     }
