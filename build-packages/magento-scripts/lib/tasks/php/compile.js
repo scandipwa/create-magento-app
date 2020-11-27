f100b74246ad38312e63ef849211a7aeb785b30b
ejnshtein <ejnshtein@gmail.com>
2020-11-27 18:24:42 +0200

    new php compile command

--- build-packages/magento-scripts/lib/tasks/php/compile.js
+++ build-packages/magento-scripts/lib/tasks/php/compile.js
@@ -2,36 +2,99 @@
 const os = require('os');
 const osPlatform = require('../../util/os-platform');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
-// const macosVersion = require('macos-version');
 const { execAsyncSpawn } = require('../../util/exec-async-command');
+
+const compileOptions = {
+    linux: {
+        cpuCount: '$(nproc)',
+        variants: [
+            '+bz2',
+            '+bcmath',
+            '+ctype',
+            '+curl',
+            '+intl',
+            '+dom',
+            '+filter',
+            '+hash',
+            '+sockets',
+            '+iconv',
+            '+json',
+            '+mbstring',
+            '+openssl',
+            '+xml',
+            '+mysql',
+            '+pdo',
+            '+soap',
+            '+xmlrpc',
+            '+xml',
+            '+zip',
+            '+fpm',
+            '+gd'
+        ],
+        extraOptions: [
+            '--with-freetype-dir=/usr/include/freetype2',
+            '--with-openssl=/usr/',
+            '--with-gd=shared',
+            '--with-jpeg-dir=/usr/',
+            '--with-png-dir=/usr/'
+        ]
+    },
+    darwin: {
+        cpuCount: '$(sysctl -n hw.ncpu)',
+        variants: [
+            '+neutral',
+            '+bz2="$(brew --prefix bzip2)"',
+            '+bcmath',
+            '+ctype',
+            '+curl=$(brew --prefix curl-openssl)',
+            '+intl=$(brew --prefix icu4c)',
+            '+dom',
+            '+filter',
+            '+hash',
+            '+iconv',
+            '+json',
+            '+mbstring',
+            '+openssl=$(brew --prefix openssl)', // ="$(brew --prefix openssl)"
+            '+xml',
+            '+mysql',
+            '+pdo',
+            '+soap',
+            '+xmlrpc',
+            '+xml',
+            '+zip',
+            '+fpm',
+            '+gd'
+        ],
+        extraOptions: [
+            '--with-zlib-dir=$(brew --prefix zlib)',
+            '--with-iconv=$(brew --prefix libiconv)',
+            '--with-gd=shared'
+        ],
+        env: {
+            // eslint-disable-next-line max-len
+            PKG_CONFIG_PATH: '$PKG_CONFIG_PATH:$(brew --prefix libxml2)/lib/pkgconfig:$(brew --prefix icu4c)/lib/pkgconfig:$(brew --prefix openssl)/lib/pkgconfig:$(brew --prefix curl-openssl)/lib/pkgconfig:$(brew --prefix zlib)/lib/pkgconfig',
+            // CPPFLAGS: '$CPPFLAGS:"-I$(brew --prefix openssl)/include":"-I$(brew --prefix libedit)/include"',
+            // LD_LIBRARY_PATH: '$LD_LIBRARY_PATH:$(brew --prefix openssl)/lib',
+            CPATH: '$CPATH:$(brew --prefix openssl)/include'
+        }
+    }
+};
 
 const compile = {
     title: 'Compiling PHP',
     task: async ({ config: { php } }, task) => {
-        let phpCompileCommand;
-        if (os.platform() === 'darwin') {
-            phpCompileCommand = `export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/opt/icu4c/lib/pkgconfig && \
-            export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/opt/openssl@1.1/lib/pkgconfig && \
-            phpbrew install -j $(sysctl -n hw.ncpu) ${php.version} +bz2="$(brew --prefix bzip2)" \
-            +bcmath +ctype +curl +intl=$(brew --prefix icu4c) +dom +filter +hash +json +mbstring +openssl="$(brew --prefix openssl)" +xml \
-            +mysql +pdo +soap +xmlrpc +xml \
-            +zip +fpm +gd -- \
-            --with-zlib-dir=$(brew --prefix zlib) \
-            --with-iconv=$(brew --prefix libiconv) \
-            --enable-zip \
-            --with-gd=shared`;
-        } else {
-            phpCompileCommand = `phpbrew install -j $(nproc) ${ php.version } \
-        +bz2 +bcmath +ctype +curl +intl +dom +filter +hash +sockets \
-        +iconv +json +mbstring +openssl +xml +mysql \
-        +pdo +soap +xmlrpc +xml +zip +fpm +gd \
-        -- --with-freetype-dir=/usr/include/freetype2 --with-openssl=/usr/ \
-        --with-gd=shared --with-jpeg-dir=/usr/ --with-png-dir=/usr/`;
+        const platformCompileOptions = compileOptions[os.platform()];
+        if (os.platform() === 'linux') {
             const { dist } = await osPlatform();
             if (dist.includes('Manjaro')) {
-                phpCompileCommand += ' --with-libdir=lib64';
+                platformCompileOptions.extraOptions.push('--with-libdir=lib64');
             }
         }
+        const exportEnv = Object.entries(platformCompileOptions.env || {}).map(([key, value]) => `export ${key}=${value}`).join(' && ');
+        const phpCompileCommand = `${exportEnv ? `${exportEnv} && ` : ''} \
+        phpbrew install -j ${platformCompileOptions.cpuCount} ${php.version} ${platformCompileOptions.variants.join(' ')} \
+        -- ${platformCompileOptions.extraOptions.join(' ')}`;
+
         try {
             await execAsyncSpawn(
                 phpCompileCommand,
@@ -51,10 +114,6 @@ const compile = {
                 Most probably you are missing some dependencies.
                 See error details in the output below.\n\n${e}`
             );
-
-            // logger.note(
-            //     'We would appreciate an issue on GitHub :)'
-            // );
         }
     },
     options: {
