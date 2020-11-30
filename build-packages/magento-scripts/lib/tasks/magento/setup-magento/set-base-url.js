955c52c6b29721950445882697f743acd03cb4ee
ejnshtein <ejnshtein@gmail.com>
2020-11-30 17:31:15 +0200

    use base-url without a port if it's 80 port

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
@@ -3,8 +3,9 @@ const runMagentoCommand = require('../../../util/run-magento');
 module.exports = {
     title: 'Setting baseurl and secure baseurl',
     task: async ({ ports, magentoVersion }) => {
-        await runMagentoCommand(`setup:store-config:set --base-url="http://localhost:${ ports.app }"`, { magentoVersion });
-        await runMagentoCommand(`setup:store-config:set --base-url-secure="https://localhost:${ ports.app }"`, { magentoVersion });
+        const url = `localhost${ ports.app !== 80 ? `:${ports.app}` : '' }`;
+        await runMagentoCommand(`setup:store-config:set --base-url="http://${url}"`, { magentoVersion });
+        await runMagentoCommand(`setup:store-config:set --base-url-secure="https://${url}"`, { magentoVersion });
         await runMagentoCommand('setup:store-config:set --use-secure=0', { magentoVersion });
         await runMagentoCommand('setup:store-config:set --use-secure-admin=0', { magentoVersion });
     }
