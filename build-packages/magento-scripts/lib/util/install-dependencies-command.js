e55233f2c617e23fce5b82ec7ec14cba33f078a2
ejnshtein <ejnshtein@gmail.com>
2020-12-02 16:07:53 +0200

    pretty print for install deps command in compile error

--- build-packages/magento-scripts/lib/util/install-dependencies-command.js
+++ build-packages/magento-scripts/lib/util/install-dependencies-command.js
@@ -4,64 +4,67 @@ const os = require('os');
 
 const getInstallDependenciesCommand = async () => {
     switch (os.platform()) {
+    /**
+         * Using array for pretty arrangement and pretty print later in logs.
+         */
     case 'darwin': {
-        return 'brew install zlib \
-        bzip2 \
-        libiconv \
-        curl \
-        libpng \
-        gd \
-        freetype \
-        oniguruma \
-        icu4c';
+        return `brew install ${ ['zlib',
+            'bzip2',
+            'libiconv',
+            'curl',
+            'libpng',
+            'gd',
+            'freetype',
+            'oniguruma',
+            'icu4c'].join(' ')}`;
     }
     case 'linux': {
         const { dist } = await osPlatform();
         switch (dist) {
         case 'Arch Linux':
         case 'Manjaro': {
-            return 'pamac install freetype2 \
-            lib32-freetype2 \
-            openssl \
-            oniguruma';
+            return `pamac install ${ ['freetype2',
+                'lib32-freetype2',
+                'openssl',
+                'oniguruma'].join(' ')}`;
         }
         case 'Fedora':
         case 'CentOS': {
-            return 'yum install --enablerepo=PowerTools openssl-devel \
-            libjpeg-turbo-devel \
-            libpng-devel \
-            gd-devel \
-            libicu libicu-devel \
-            libzip-devel \
-            libtool-ltdl-devel \
-            oniguruma-devel';
+            return `yum install ${ ['--enablerepo=PowerTools', 'openssl-devel',
+                'libjpeg-turbo-devel',
+                'libpng-devel',
+                'gd-devel',
+                'libicu libicu-devel',
+                'libzip-devel',
+                'libtool-ltdl-devel',
+                'oniguruma-devel'].join(' ')}`;
         }
         case 'Linux Mint':
         case 'Ubuntu':
         default: {
-            return 'apt-get install \
-                libcurl4-openssl-dev \
-                libonig-dev \
-                libjpeg-dev \
-                libjpeg8-dev \
-                libjpeg-turbo8-dev \
-                libpng-dev \
-                libicu-dev \
-                libfreetype6-dev \
-                libzip-dev \
-                libssl-dev \
-                build-essential \
-                libbz2-dev \
-                libreadline-dev \
-                libsqlite3-dev \
-                libssl-dev \
-                libxml2-dev \
-                libxslt-dev \
-                libonig-dev \
-                php-cli \
-                php-bz2 \
-                pkg-config \
-                autoconf';
+            return `apt-get install ${ [
+                'libcurl4-openssl-dev',
+                'libonig-dev',
+                'libjpeg-dev',
+                'libjpeg8-dev',
+                'libjpeg-turbo8-dev',
+                'libpng-dev',
+                'libicu-dev',
+                'libfreetype6-dev',
+                'libzip-dev',
+                'libssl-dev',
+                'build-essential',
+                'libbz2-dev',
+                'libreadline-dev',
+                'libsqlite3-dev',
+                'libssl-dev',
+                'libxml2-dev',
+                'libxslt-dev',
+                'libonig-dev',
+                'php-cli',
+                'php-bz2',
+                'pkg-config',
+                'autoconf'].join(' ')}`;
         }
         }
     }
