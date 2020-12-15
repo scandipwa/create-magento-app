c775b45998a46f13b092626cf33e5117bacb622e
ejnshtein <ejnshtein@gmail.com>
2020-12-15 16:19:39 +0200

    use config set for baseurl setting

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
@@ -4,9 +4,10 @@ module.exports = {
     title: 'Setting baseurl and secure baseurl',
     task: async ({ ports, magentoVersion }) => {
         const url = `localhost${ ports.app !== 80 ? `:${ports.app}` : '' }`;
-        await runMagentoCommand(`setup:store-config:set --base-url="http://${url}"`, { magentoVersion });
-        await runMagentoCommand(`setup:store-config:set --base-url-secure="https://${url}"`, { magentoVersion });
-        await runMagentoCommand('setup:store-config:set --use-secure=0', { magentoVersion });
-        await runMagentoCommand('setup:store-config:set --use-secure-admin=0', { magentoVersion });
+        // config:set with path web/unsecure/base_url
+        await runMagentoCommand(`config:set web/unsecure/base_url http://${url}`, { magentoVersion });
+        await runMagentoCommand(`config:set web/secure/base_url https://${url}`, { magentoVersion });
+        await runMagentoCommand('config:set web/secure/use_in_frontend 0', { magentoVersion });
+        await runMagentoCommand('config:set web/secure/use_in_adminhtml 0', { magentoVersion });
     }
 };
