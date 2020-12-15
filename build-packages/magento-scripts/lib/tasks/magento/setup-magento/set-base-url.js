41677cad53dfaa06c5d969e2096f239956ec792a
ejnshtein <ejnshtein@gmail.com>
2020-12-15 16:25:36 +0200

    add slash for base_url

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/set-base-url.js
@@ -3,7 +3,7 @@ const runMagentoCommand = require('../../../util/run-magento');
 module.exports = {
     title: 'Setting baseurl and secure baseurl',
     task: async ({ ports, magentoVersion }) => {
-        const url = `localhost${ ports.app !== 80 ? `:${ports.app}` : '' }`;
+        const url = `localhost${ ports.app !== 80 ? `:${ports.app}` : '' }/`;
         // config:set with path web/unsecure/base_url
         await runMagentoCommand(`config:set web/unsecure/base_url http://${url}`, { magentoVersion });
         await runMagentoCommand(`config:set web/secure/base_url https://${url}`, { magentoVersion });
