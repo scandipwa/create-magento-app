e592ddc245938154dcafb60c405fc12f612846f2
ejnshtein <ejnshtein@gmail.com>
2020-12-18 18:28:48 +0200

    save ports using it's home function

--- build-packages/magento-scripts/lib/tasks/file-system/create-port-config.js
+++ build-packages/magento-scripts/lib/tasks/file-system/create-port-config.js
@@ -1,17 +1,10 @@
-const path = require('path');
-const { config } = require('../../config');
-const setConfigFile = require('../../util/set-config');
+const { savePortsConfig } = require('../../util/ports');
 
 const createPortConfig = {
     title: 'Setting port config',
     task: async ({ ports }) => {
         try {
-            await setConfigFile({
-                configPathname: path.join(config.cacheDir, 'port-config.json'),
-                template: path.join(config.templateDir, 'port-config.template.json'),
-                ports,
-                overwrite: true
-            });
+            await savePortsConfig(ports);
         } catch (e) {
             throw new Error(`Unexpected error accrued during port config creation\n\n${e}`);
         }
