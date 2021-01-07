7bf9107fa171c30c180273181f1c5a600fe087e2
ejnshtein <ejnshtein@gmail.com>
2021-01-07 17:31:49 +0200

    use system host for redis-cli to flush redis db

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/flush-redis-config.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/flush-redis-config.js
@@ -1,12 +1,14 @@
 const { execAsyncSpawn } = require('../../../util/exec-async-command');
+const macosVersion = require('macos-version');
 
 module.exports = {
     title: 'Flushing Magento redis cache',
     task: async ({ ports, config: { docker } }) => {
         const { redis: { name } } = docker.getContainers(ports);
-        const result = await execAsyncSpawn(`docker exec -t ${ name } redis-cli -h ${ name } -n 0 flushdb`);
+        const host = macosVersion.isMacOS ? 'host.docker.internal' : '127.0.0.1';
+        const result = await execAsyncSpawn(`docker exec ${ name } redis-cli -h ${ host } -p ${ ports.redis } -n 0 flushdb`);
 
-        if (!result.includes('OK')) {
+        if (!result.trim().includes('OK')) {
             throw new Error(`Unexpected output from redis flush command: ${result}`);
         }
     }
