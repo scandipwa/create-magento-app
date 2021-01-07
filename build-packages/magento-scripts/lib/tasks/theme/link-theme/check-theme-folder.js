1872ca40d3f8397a0a2753cdaff51fe47537299b
ejnshtein <ejnshtein@gmail.com>
2021-01-07 17:17:33 +0200

    support relative theme path in composer repository symlink

--- build-packages/magento-scripts/lib/tasks/theme/link-theme/check-theme-folder.js
+++ build-packages/magento-scripts/lib/tasks/theme/link-theme/check-theme-folder.js
@@ -17,7 +17,20 @@ const checkThemeFolder = {
         const { themepath } = ctx;
         task.output = 'Checking theme folder';
 
-        const absoluteThemePath = path.join(process.cwd(), themepath);
+        let absoluteThemePath = path.join(process.cwd(), themepath);
+
+        // check if path not relative
+        if (!(await pathExists(path.join(absoluteThemePath, 'composer.json')))) {
+            // if composer.json is not found, then it's not correct path
+            // and we need to test if it's alsolute one
+            if (await pathExists(path.join(themepath, 'composer.json'))) {
+                // if so, use it as absolute path
+                absoluteThemePath = themepath;
+            }
+        } else {
+            // path is relative, so we use it
+            absoluteThemePath = themepath;
+        }
 
         const composerData = await getComposerData(path.join(absoluteThemePath, 'composer.json'));
 
