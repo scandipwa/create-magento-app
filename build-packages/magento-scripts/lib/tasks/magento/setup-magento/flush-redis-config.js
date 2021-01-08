7c9a33e6f56aed69aa651a2a6e13158401c530d8
ejnshtein <ejnshtein@gmail.com>
2021-01-08 18:10:09 +0200

    fix magento flush for linux

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/flush-redis-config.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/flush-redis-config.js
@@ -5,8 +5,9 @@ module.exports = {
     title: 'Flushing Magento redis cache',
     task: async ({ ports, config: { docker } }) => {
         const { redis: { name } } = docker.getContainers(ports);
-        const host = macosVersion.isMacOS ? 'host.docker.internal' : '127.0.0.1';
-        const result = await execAsyncSpawn(`docker exec ${ name } redis-cli -h ${ host } -p ${ ports.redis } -n 0 flushdb`);
+        const host = macosVersion.isMacOS ? 'host.docker.internal' : 'localhost';
+        const port = macosVersion.isMacOS ? ports.redis : '6379';
+        const result = await execAsyncSpawn(`docker exec ${ name } redis-cli -h ${ host } -p ${ port } -n 0 flushdb`);
 
         if (!result.trim().includes('OK')) {
             throw new Error(`Unexpected output from redis flush command: ${result}`);
