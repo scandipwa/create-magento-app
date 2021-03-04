5790e2781129987bed6268ca37fd24b76ea62970
ejnshtein <ejnshtein@gmail.com>
2021-03-04 14:19:06 +0200

    add configure ES check for the port configuration

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/configure-elasticsearch.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/configure-elasticsearch.js
@@ -2,10 +2,11 @@ const runMagentoCommand = require('../../../util/run-magento');
 
 module.exports = {
     title: 'Configuring elasticsearch',
-    task: async ({ magentoVersion }, task) => {
+    task: async ({ magentoVersion, ports }, task) => {
         const [
             { result: catalogSearchEngine },
-            { result: catalogSearchElasticsearch7ServerHostname }
+            { result: catalogSearchElasticsearch7ServerHostname },
+            { result: catalogSearchElasticsearch7ServerPort }
         ] = await Promise.all([
             runMagentoCommand('config:show catalog/search/engine', {
                 throwNonZeroCode: false,
@@ -14,22 +15,36 @@ module.exports = {
             runMagentoCommand('config:show catalog/search/elasticsearch7_server_hostname', {
                 throwNonZeroCode: false,
                 magentoVersion
+            }),
+            runMagentoCommand('config:show catalog/search/elasticsearch7_server_port', {
+                throwNonZeroCode: false,
+                magentoVersion
             })
         ]);
 
         if (
             catalogSearchEngine === 'elasticsearch7'
             && catalogSearchElasticsearch7ServerHostname === '127.0.0.1'
+            && catalogSearchElasticsearch7ServerPort === ports.elasticsearch
         ) {
             task.skip();
             return;
         }
 
-        await runMagentoCommand('config:set catalog/search/engine elasticsearch7', {
-            magentoVersion
-        });
-        await runMagentoCommand('config:set catalog/search/elasticsearch7_server_hostname 127.0.0.1', {
-            magentoVersion
-        });
+        if (catalogSearchEngine !== 'elasticsearch7') {
+            await runMagentoCommand('config:set catalog/search/engine elasticsearch7', {
+                magentoVersion
+            });
+        }
+        if (catalogSearchElasticsearch7ServerHostname !== '127.0.0.1') {
+            await runMagentoCommand('config:set catalog/search/elasticsearch7_server_hostname 127.0.0.1', {
+                magentoVersion
+            });
+        }
+        if (catalogSearchElasticsearch7ServerPort !== ports.elasticsearch) {
+            await runMagentoCommand(`config:set catalog/search/elasticsearch7_server_port ${ports.elasticsearch}`, {
+                magentoVersion
+            });
+        }
     }
 };
