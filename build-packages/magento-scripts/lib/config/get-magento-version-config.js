355f686c2edebcc29ab5f5fc5ce8e479acce1cf9
ejnshtein <ejnshtein@gmail.com>
2021-03-10 20:19:24 +0200

    fix magento version picker, magento 2.4.2 support soon, hopefully

--- build-packages/magento-scripts/lib/config/get-magento-version-config.js
+++ build-packages/magento-scripts/lib/config/get-magento-version-config.js
@@ -8,51 +8,50 @@ const getMagentoVersion = {
     // title: 'Getting magento version (300 sec left...)',
     title: 'Loading Magento version',
     task: async (ctx, task) => {
-        /**
-         * @type {String}
-         */
-        let magentoVersion;
+        let { magentoVersion } = ctx;
 
-        try {
-            magentoVersion = await getInstalledMagentoVersion();
-        } catch (e) {
-            if (ctx.throwMagentoVersionMissing) {
-                throw e;
-            }
-            if (allVersions.length === 1) {
-                const { magentoVersion: defaultMagentoVersion } = defaultConfiguration;
-                magentoVersion = defaultMagentoVersion;
-            } else {
-                let promptSkipper = false;
-                const timer = async () => {
-                    for (let i = 5 * 60; i !== 0; i--) {
-                        // eslint-disable-next-line no-await-in-loop
-                        await sleep(1000);
-                        if (promptSkipper) {
-                            return null;
-                        }
-                        task.title = `Checking app config (${i} sec left...)`;
-                    }
-                    task.cancelPrompt();
-                    return defaultConfiguration.magento;
-                };
-
-                magentoVersion = await Promise.race([
-                    task.prompt({
-                        type: 'Select',
-                        message: 'Choose Magento Version',
-                        name: 'magentoVersion',
-                        choices: allVersions.map(({ magentoVersion }) => (
-                            {
-                                name: magentoVersion,
-                                message: magentoVersion
+        if (!magentoVersion) {
+            try {
+                magentoVersion = await getInstalledMagentoVersion();
+            } catch (e) {
+                if (ctx.throwMagentoVersionMissing) {
+                    throw e;
+                }
+                if (allVersions.length === 1) {
+                    const { magentoVersion: defaultMagentoVersion } = defaultConfiguration;
+                    magentoVersion = defaultMagentoVersion;
+                } else {
+                    let promptSkipper = false;
+                    const timer = async () => {
+                        for (let i = 5 * 60; i !== 0; i--) {
+                            // eslint-disable-next-line no-await-in-loop
+                            await sleep(1000);
+                            if (promptSkipper) {
+                                return null;
                             }
-                        ))
-                    }),
-                    timer()
-                ]);
+                            task.title = `Checking app config (${i} sec left...)`;
+                        }
+                        task.cancelPrompt();
+                        return defaultConfiguration.magento;
+                    };
 
-                promptSkipper = true;
+                    magentoVersion = await Promise.race([
+                        task.prompt({
+                            type: 'Select',
+                            message: 'Choose Magento Version',
+                            name: 'magentoVersion',
+                            choices: allVersions.map((version) => (
+                                {
+                                    name: version.magentoVersion,
+                                    message: version.magentoVersion
+                                }
+                            ))
+                        }),
+                        timer()
+                    ]);
+
+                    promptSkipper = true;
+                }
             }
         }
 
