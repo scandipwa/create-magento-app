b4f4852904fed9c12da7d79b428074eaebdbb95c
ejnshtein <ejnshtein@gmail.com>
2021-03-10 15:38:33 +0200

    update php modules configuration mechanism, now it should properly identify which additional modules and with which version is requred to be used by PHP

--- build-packages/magento-scripts/lib/tasks/php/configure.js
+++ build-packages/magento-scripts/lib/tasks/php/configure.js
@@ -2,12 +2,33 @@
 const { execAsyncSpawn } = require('../../util/exec-async-command');
 const macosVersion = require('macos-version');
 
+/**
+ * Get installed modules list with versions
+ * @param {*} param0
+ * @param {Object} param0.php
+ * @returns {Promise<{[key: string]: string}}>}
+ */
+const getInstalledModules = async ({ php }) => {
+    const output = await execAsyncSpawn(`${ php.binPath } -c ${php.iniPath} -r 'foreach (get_loaded_extensions() as $extension) echo "$extension:" . phpversion($extension) . "\n";'`);
+
+    return output
+        .split('\n')
+        .map((m) => {
+            // eslint-disable-next-line no-unused-vars
+            const [_, moduleName, moduleVersion] = m.match(/(.+):(.+)/i);
+
+            return [moduleName, moduleVersion];
+        })
+        .reduce((acc, [name, version]) => ({ ...acc, [name]: version }), {});
+};
+
 const configure = {
     title: 'Configuring PHP extensions',
     task: async ({ config: { php } }, task) => {
-        const loadedModules = await execAsyncSpawn(`${ php.binPath } -c ${php.iniPath} -m`);
+        const loadedModules = await getInstalledModules({ php });
         const missingExtensions = Object.entries(php.extensions)
-            .filter(([name]) => !loadedModules.includes(name));
+            // check if module is not loaded and if it is loaded check installed version
+            .filter(([name, options]) => !loadedModules[name] || (options && options.version && loadedModules[name] !== options.version));
 
         if (missingExtensions.length === 0) {
         // if all extensions are installed - do not configure PHP
