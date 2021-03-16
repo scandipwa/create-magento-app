9ea415ed323cc9e7a6775bb85428865aef944e75
ejnshtein <ejnshtein@gmail.com>
2021-03-16 14:57:40 +0200

    update env.php only if it exists

--- build-packages/magento-scripts/lib/tasks/php/update-env-php.js
+++ build-packages/magento-scripts/lib/tasks/php/update-env-php.js
@@ -1,16 +1,22 @@
 /* eslint-disable no-param-reassign */
 const path = require('path');
+const pathExists = require('../../util/path-exists');
 const runPhpCode = require('../../util/run-php');
 
 const updateEnvPHP = {
     title: 'Updating env.php',
     task: async (ctx, task) => {
-        await runPhpCode(`-f ${ path.join(__dirname, 'update-env.php') }`, {
-            callback: (t) => {
-                task.output = t;
-            },
-            throwNonZeroCode: true
-        });
+        // update env.php only if it's exist
+        if (await pathExists(path.resolve('app', 'etc', 'env.php'))) {
+            await runPhpCode(`-f ${ path.join(__dirname, 'update-env.php') }`, {
+                callback: (t) => {
+                    task.output = t;
+                },
+                throwNonZeroCode: true
+            });
+        } else {
+            task.skip();
+        }
     }
 };
 
