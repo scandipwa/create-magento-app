e00be8a3a875a0bdb3a2e2a4e4c427ab4c04e84c
ejnshtein <ejnshtein@gmail.com>
2021-03-23 18:16:06 +0200

    add tries to magento setup:install command to make sure that it will install on empty database

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/install-magento.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/install-magento.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-param-reassign */
+/* eslint-disable no-await-in-loop,no-param-reassign */
 const runMagentoCommand = require('../../../util/run-magento');
 
 const installMagento = {
@@ -13,36 +13,49 @@ const installMagento = {
             ports
         } = ctx;
         const { mysql: { env } } = docker.getContainers(ports);
-        await runMagentoCommand(`setup:install \
-        --admin-firstname='${ magentoConfiguration.first_name }' \
-        --admin-lastname='${ magentoConfiguration.last_name }' \
-        --admin-email='${ magentoConfiguration.email }' \
-        --admin-user='${ magentoConfiguration.user }' \
-        --admin-password='${ magentoConfiguration.password }' \
-        --search-engine='elasticsearch7' \
-        --elasticsearch-host='127.0.0.1' \
-        --elasticsearch-port='${ ports.elasticsearch }' \
-        --session-save=redis \
-        --session-save-redis-host='127.0.0.1' \
-        --session-save-redis-port='${ ports.redis }' \
-        --session-save-redis-log-level='3' \
-        --session-save-redis-max-concurrency='30' \
-        --session-save-redis-db='1' \
-        --session-save-redis-disable-locking='1' \
-        --cache-backend='redis' \
-        --cache-backend-redis-server='127.0.0.1' \
-        --cache-backend-redis-port='${ ports.redis }' \
-        --cache-backend-redis-db='0't \
-        --db-host='127.0.0.1:${ ports.mysql }' \
-        --db-name='${ env.MYSQL_DATABASE }' \
-        --db-user='${ env.MYSQL_USER }' \
-        --db-password='${ env.MYSQL_PASSWORD }' \
-        --backend-frontname='${ magentoConfiguration.adminuri }'`, {
-            magentoVersion,
-            callback: (t) => {
-                task.output = t;
+
+        for (let tries = 0; tries < 2; tries++) {
+            try {
+                const command = `setup:install \
+                --admin-firstname='${ magentoConfiguration.first_name }' \
+                --admin-lastname='${ magentoConfiguration.last_name }' \
+                --admin-email='${ magentoConfiguration.email }' \
+                --admin-user='${ magentoConfiguration.user }' \
+                --admin-password='${ magentoConfiguration.password }' \
+                --search-engine='elasticsearch7' \
+                --elasticsearch-host='127.0.0.1' \
+                --elasticsearch-port='${ ports.elasticsearch }' \
+                --session-save=redis \
+                --session-save-redis-host='127.0.0.1' \
+                --session-save-redis-port='${ ports.redis }' \
+                --session-save-redis-log-level='3' \
+                --session-save-redis-max-concurrency='30' \
+                --session-save-redis-db='1' \
+                --session-save-redis-disable-locking='1' \
+                --cache-backend='redis' \
+                --cache-backend-redis-server='127.0.0.1' \
+                --cache-backend-redis-port='${ ports.redis }' \
+                --cache-backend-redis-db='0't \
+                --db-host='127.0.0.1:${ ports.mysql }' \
+                --db-name='${ env.MYSQL_DATABASE }' \
+                --db-user='${ env.MYSQL_USER }' \
+                --db-password='${ env.MYSQL_PASSWORD }' \
+                --backend-frontname='${ magentoConfiguration.adminuri }' \
+                --cleanup-database`;
+
+                await runMagentoCommand(command, {
+                    magentoVersion,
+                    throwNonZeroCode: true,
+                    callback: (t) => {
+                        task.output = t;
+                    }
+                });
+            } catch (e) {
+                if (tries === 2) {
+                    throw e;
+                }
             }
-        });
+        }
 
         await runMagentoCommand('cache:enable', {
             magentoVersion,
