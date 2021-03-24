4b1e33b5f01ba2a871c9a67775097eb1ae6f9764
ejnshtein <ejnshtein@gmail.com>
2021-03-24 18:36:12 +0200

    add typings to os-platform utility

--- build-packages/magento-scripts/lib/util/os-platform.js
+++ build-packages/magento-scripts/lib/util/os-platform.js
@@ -1,5 +1,9 @@
 const getos = require('getos');
 
+/**
+ *
+ * @returns {Promise<import('getos').OS>}
+ */
 const osPlatform = () => new Promise((resolve, reject) => getos((err, os) => (err ? reject(err) : resolve(os))));
 
 module.exports = osPlatform;
