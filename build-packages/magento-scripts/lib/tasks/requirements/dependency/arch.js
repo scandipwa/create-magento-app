1bb1b877109feaab4e88e6dcdb3244b4e78c5a59
ejnshtein <ejnshtein@gmail.com>
2021-03-26 18:16:06 +0200

    add arch linux and ubuntu dependencies install

--- build-packages/magento-scripts/lib/tasks/requirements/dependency/arch.js
+++ build-packages/magento-scripts/lib/tasks/requirements/dependency/arch.js
@@ -1,6 +1,8 @@
+/* eslint-disable consistent-return,no-param-reassign */
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const dependenciesForPlatforms = require('../../../config/dependencies-for-platforms');
-const { execAsyncSpawn } = require('../../../util/exec-async-command');
+const { execAsyncSpawn, execCommandTask } = require('../../../util/exec-async-command');
+const sleep = require('../../../util/sleep');
 
 const pkgRegex = /(\S+)\s(\S+)/i;
 
@@ -9,7 +11,7 @@ const pkgRegex = /(\S+)\s(\S+)/i;
  */
 const archDependenciesCheck = {
     title: 'Checking Arch Linux dependencies',
-    task: async () => {
+    task: async (ctx, task) => {
         const installedDependencies = (await execAsyncSpawn('pacman -Qe')).split('\n')
             .map((pkg) => {
                 const result = pkg.match(pkgRegex);
@@ -18,14 +20,75 @@ const archDependenciesCheck = {
                     throw new Error(`Package without a version!\n\n${pkg}\n\nHOW?`);
                 }
 
-                return [result[1], result[2]];
+                return result[1];
             });
 
-        const dependenciesToInstall = dependenciesForPlatforms['Arch Linux'].filter((dep) => !installedDependencies.some((pkg) => pkg[0] === dep));
+        const dependenciesToInstall = dependenciesForPlatforms['Arch Linux'].filter((dep) => !installedDependencies.some((pkg) => pkg === dep));
 
+        // if (dependenciesToInstall.length > 0) {
+        //     throw new Error(`Missing dependencies detected!\n\nYou can install them by running the following command: ${ logger.style.code(`pamac install ${dependenciesToInstall.join(' ') }`)}`);
+        // }
         if (dependenciesToInstall.length > 0) {
-            throw new Error(`Missing dependencies detected!\n\nYou can install them by running the following command: ${ logger.style.code(`pamac install ${dependenciesToInstall.join(' ') }`)}`);
+            const installCommand = logger.style.code(`pamac install ${dependenciesToInstall.join(' ') }`);
+            const dependenciesWordFormatter = `dependenc${dependenciesToInstall.length > 1 ? 'ies' : 'y'}`;
+            task.output = `Missing ${ dependenciesWordFormatter } ${ logger.style.code(dependenciesToInstall.join(' ')) } detected!`;
+
+            let promptSkipper = false;
+            const timer = async () => {
+                for (let i = 5 * 60; i !== 0; i--) {
+                    // eslint-disable-next-line no-await-in-loop
+                    await sleep(1000);
+                    if (promptSkipper) {
+                        return null;
+                    }
+                    task.title = `Checking MacOS dependencies (${i} sec left...)`;
+                }
+                task.cancelPrompt();
+                return 'timeout';
+            };
+
+            const installAnswer = await Promise.race([
+                task.prompt({
+                    type: 'Select',
+                    message: `Do you want to install missing ${ dependenciesWordFormatter } now?`,
+                    name: 'installAnswer',
+                    choices: [
+                        {
+                            name: 'install',
+                            message: `Install ${ dependenciesWordFormatter } now!`
+                        },
+                        {
+                            name: 'not-install',
+                            message: `Do not install ${ dependenciesWordFormatter } now!`
+                        }
+                    ]
+                }),
+                timer()
+            ]);
+
+            promptSkipper = true;
+
+            if (installAnswer === 'timeout') {
+                throw new Error(`User timeout.
+
+You need to install missing ${ dependenciesWordFormatter } manually, run the following command: ${ installCommand }`);
+            }
+
+            if (installAnswer === 'not-install') {
+                throw new Error(`User chosen to not install ${ dependenciesWordFormatter } now.
+
+You need to install missing ${ dependenciesWordFormatter } manually, run the following command: ${ installCommand }`);
+            }
+
+            if (installAnswer === 'install') {
+                return task.newListr([
+                    execCommandTask(`brew install ${dependenciesToInstall.join(' ') }`)
+                ]);
+            }
         }
+    },
+    options: {
+        bottomBar: 10
     }
 };
 
--- build-packages/magento-scripts/lib/tasks/requirements/dependency/ubuntu.js
+++ build-packages/magento-scripts/lib/tasks/requirements/dependency/ubuntu.js
@@ -1,6 +1,9 @@
+/* eslint-disable consistent-return,no-param-reassign */
+const os = require('os');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const dependenciesForPlatforms = require('../../../config/dependencies-for-platforms');
-const { execAsyncSpawn } = require('../../../util/exec-async-command');
+const { execAsyncSpawn, execCommandTask } = require('../../../util/exec-async-command');
+const sleep = require('../../../util/sleep');
 
 const pkgRegex = /^(\S+)\/\S+\s(\S+)\s\S+\s\S+$/i;
 
@@ -9,7 +12,7 @@ const pkgRegex = /^(\S+)\/\S+\s(\S+)\s\S+\s\S+$/i;
  */
 const ubuntuDependenciesCheck = {
     title: 'Checking Ubuntu Linux dependencies',
-    task: async () => {
+    task: async (ctx, task) => {
         const installedDependencies = (await execAsyncSpawn('apt list --installed')).split('\n')
             .filter((pkg) => pkgRegex.test(pkg))
             .map((pkg) => pkg.match(pkgRegex))
@@ -25,9 +28,77 @@ const ubuntuDependenciesCheck = {
                 return !installedDependencies.includes(dep);
             });
 
+        // if (dependenciesToInstall.length > 0) {
+        //     throw new Error(`Missing dependencies detected!\n\nYou can install them by running the following command: ${ logger.style.code(`apt-get install ${dependenciesToInstall.map((dep) => (Array.isArray(dep) ? dep[0] : dep)).join(' ') }`)}`);
+        // }
         if (dependenciesToInstall.length > 0) {
-            throw new Error(`Missing dependencies detected!\n\nYou can install them by running the following command: ${ logger.style.code(`apt-get install ${dependenciesToInstall.map((dep) => (Array.isArray(dep) ? dep[0] : dep)).join(' ') }`)}`);
+            const cmd = `sudo apt-get install ${dependenciesToInstall.map((dep) => (Array.isArray(dep) ? dep[0] : dep)).join(' ') }`;
+            const installCommand = logger.style.code(cmd);
+            const dependenciesWordFormatter = `dependenc${dependenciesToInstall.length > 1 ? 'ies' : 'y'}`;
+            task.output = `Missing ${ dependenciesWordFormatter } ${ logger.style.code(dependenciesToInstall.join(' ')) } detected!`;
+
+            let promptSkipper = false;
+            const timer = async () => {
+                for (let i = 5 * 60; i !== 0; i--) {
+                    // eslint-disable-next-line no-await-in-loop
+                    await sleep(1000);
+                    if (promptSkipper) {
+                        return null;
+                    }
+                    task.title = `Checking Ubuntu dependencies (${i} sec left...)`;
+                }
+                task.cancelPrompt();
+                return 'timeout';
+            };
+
+            const installAnswer = await Promise.race([
+                task.prompt({
+                    type: 'Select',
+                    message: `Do you want to install missing ${ dependenciesWordFormatter } now?`,
+                    name: 'installAnswer',
+                    choices: [
+                        {
+                            name: 'install',
+                            message: `Install ${ dependenciesWordFormatter } now!`
+                        },
+                        {
+                            name: 'not-install',
+                            message: `Do not install ${ dependenciesWordFormatter } now!`
+                        }
+                    ]
+                }),
+                timer()
+            ]);
+
+            promptSkipper = true;
+
+            if (installAnswer === 'timeout') {
+                throw new Error(`User timeout.
+
+You need to install missing ${ dependenciesWordFormatter } manually, run the following command: ${ installCommand }`);
+            }
+
+            if (installAnswer === 'not-install') {
+                throw new Error(`User chosen to not install ${ dependenciesWordFormatter } now.
+
+You need to install missing ${ dependenciesWordFormatter } manually, run the following command: ${ installCommand }`);
+            }
+
+            if (installAnswer === 'install') {
+                task.output = `[sudo] password for ${ os.userInfo().username };`;
+                return task.newListr([
+                    execCommandTask(`${ cmd } -y`, {
+                        callback: (str) => {
+                            task.output = str;
+                        },
+                        pipeInput: true
+                    })
+                ]);
+            }
         }
+    },
+    options: {
+        bottomBar: 10
     }
 };
 
