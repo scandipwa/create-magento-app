c7df8f6576869f2a155e0950c6e3c644d9fa97c0
Okabe Rintaro <ejnshtein@gmail.com>
2021-04-27 16:13:03 +0300

    add magento 2.4.1-p1 to available versions

--- build-packages/magento-scripts/lib/config/versions/index.js
+++ build-packages/magento-scripts/lib/config/versions/index.js
@@ -1,5 +1,6 @@
 const deepmerge = require('../../util/deepmerge');
 const magento241 = require('./magento-2.4.1');
+const magento241p1 = require('./magento-2.4.1-p1');
 const magento242 = require('./magento-2.4.2');
 
 const defaultCMAConfig = {
@@ -11,6 +12,9 @@ const getConfigurations = (config = {}) => ({
     '2.4.1': deepmerge(defaultCMAConfig, {
         isDefault: true,
         ...magento241(config)
+    }),
+    '2.4.1-p1': deepmerge(defaultCMAConfig, {
+        ...magento241p1(config)
     }),
     '2.4.2': deepmerge(defaultCMAConfig, {
         ...magento242(config)
---
+++ build-packages/magento-scripts/lib/config/versions/magento-2.4.1-p1.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { defaultMagentoConfig } = require('../magento-config');
+
+module.exports = ({ templateDir } = {}) => ({
+    magentoVersion: '2.4.1-p1',
+    configuration: {
+        php: {
+            version: '7.4.13',
+            configTemplate: path.join(templateDir || '', 'php.template.ini'),
+            extensions: {
+                gd: {},
+                intl: {},
+                zlib: {},
+                openssl: {},
+                sockets: {},
+                SimpleXML: {},
+                xdebug: {
+                    version: '3.0.4'
+                }
+            }
+        },
+        nginx: {
+            version: '1.18.0',
+            configTemplate: path.join(templateDir || '', 'nginx.template.conf')
+        },
+        redis: {
+            version: '6.0.10-alpine'
+        },
+        mysql: {
+            version: '8.0'
+        },
+        elasticsearch: {
+            version: '7.6.2'
+        },
+        composer: {
+            version: '1'
+        }
+    },
+    magento: defaultMagentoConfig,
+    host: 'localhost',
+    ssl: {
+        enabled: false
+    }
+});
