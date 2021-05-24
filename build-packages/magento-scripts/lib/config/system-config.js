addf3b0fff28880d6fe730524399970be8d90cda
ejnshtein <ejnshtein@gmail.com>
2021-05-24 18:21:44 +0300

    add system config resolver & validator for it, preparing for integration with main codebase

---
+++ build-packages/magento-scripts/lib/config/system-config.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-param-reassign */
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const pathExists = require('../util/path-exists');
+const { systemConfigurationSchema } = require('../util/config-file-validator');
+const deepmerge = require('../util/deepmerge');
+
+const defaultSystemConfig = {
+    analytics: true,
+    useNonOverlappingPorts: false
+};
+const systemConfigPath = path.join(os.homedir(), '.cmarc');
+
+/**
+ * Get system configuration from configuration file located in user root directory.
+ * @returns {Promise<typeof defaultSystemConfig>}
+ */
+const getSystemConfig = async () => {
+    if (await pathExists(systemConfigPath)) {
+        const userSystemConfig = await fs.promises.readFile(systemConfigPath, 'utf-8');
+        let userSystemConfigParsed;
+        try {
+            userSystemConfigParsed = JSON.parse(userSystemConfig);
+        } catch (e) {
+            throw new Error(`System configuration file is not a valid JSON!\n\nFile location: ${systemConfigPath}`);
+        }
+        try {
+            await systemConfigurationSchema.validateAsync(userSystemConfigParsed);
+        } catch (e) {
+            throw new Error(`Configuration file validation error!\n\n${e.message}`);
+        }
+
+        return deepmerge(defaultSystemConfig, userSystemConfigParsed);
+    }
+
+    return defaultSystemConfig;
+};
+
+/**
+ * Get system configuration from configuration file located in user root directory as Listr2 task.
+ * @type {import('listr2').ListrTask<import('../../typings/context').ListrContext>}
+ */
+const getSystemConfigTask = {
+    task: async (ctx) => {
+        ctx.systemConfiguration = await getSystemConfig();
+    }
+};
+
+module.exports = {
+    defaultSystemConfig,
+    getSystemConfig,
+    getSystemConfigTask
+};
