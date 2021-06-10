4b8aff3ba3b0bee7408dfbdfe998e91b455959b1
ejnshtein <ejnshtein@gmail.com>
2021-06-10 19:22:02 +0300

    stage delete customers and orders sql queries

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/delete-customers.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/delete-customers.js
@@ -1,30 +1,13 @@
 /* eslint-disable max-len */
 /**
- * @type {import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
+ * @type {import(listr2).ListrTask<import(../../../../typings/context).ListrContext>}
  */
 const deleteCustomers = {
     title: 'Deleting customers',
     task: async (ctx) => {
         const { mysqlConnection } = ctx;
         await mysqlConnection.query(`
-        ***********for categories********************
-        TRUNCATE TABLE 'catalog_category_entity';
-        TRUNCATE TABLE 'catalog_category_entity_datetime';
-        TRUNCATE TABLE 'catalog_category_entity_decimal';
-        TRUNCATE TABLE 'catalog_category_entity_int';
-        TRUNCATE TABLE 'catalog_category_entity_text';
-        TRUNCATE TABLE 'catalog_category_entity_varchar';
-        TRUNCATE TABLE 'catalog_category_product';
-        TRUNCATE TABLE 'catalog_category_product_index';
-
-        INSERT  INTO 'catalog_category_entity'('entity_id','entity_type_id','attribute_set_id','parent_id','created_at','updated_at','path','POSITION','level','children_count') VALUES (1,3,0,0,'0000-00-00 00:00:00','2009-02-20 00:25:34','1',1,0,1),(2,3,3,0,'2009-02-20 00:25:34','2009-02-20 00:25:34','1/2',1,1,0);
-        INSERT  INTO 'catalog_category_entity_int'('value_id','entity_type_id','attribute_id','store_id','entity_id','value') VALUES (1,3,32,0,2,1),(2,3,32,1,2,1);
-        INSERT  INTO 'catalog_category_entity_varchar'('value_id','entity_type_id','attribute_id','store_id','entity_id','value') VALUES (1,3,31,0,1,'Root Catalog'),(2,3,33,0,1,'root-catalog'),(3,3,31,0,2,'Default Category'),(4,3,39,0,2,'PRODUCTS'),(5,3,33,0,2,'default-category');
-
-            *****************for customers*****************
-
-            SET FOREIGN_KEY_CHECKS=0;
-        -- reset customers
+        SET FOREIGN_KEY_CHECKS=0;
         TRUNCATE customer_address_entity;
         TRUNCATE customer_address_entity_datetime;
         TRUNCATE customer_address_entity_decimal;
@@ -37,9 +20,8 @@ const deleteCustomers = {
         TRUNCATE customer_entity_int;
         TRUNCATE customer_entity_text;
         TRUNCATE customer_entity_varchar;
-        TRUNCATE log_customer;
-        TRUNCATE log_visitor;
-        TRUNCATE log_visitor_info;
+        TRUNCATE login_as_customer;
+        TRUNCATE customer_visitor;
 
         ALTER TABLE customer_address_entity AUTO_INCREMENT=1;
         ALTER TABLE customer_address_entity_datetime AUTO_INCREMENT=1;
@@ -53,9 +35,8 @@ const deleteCustomers = {
         ALTER TABLE customer_entity_int AUTO_INCREMENT=1;
         ALTER TABLE customer_entity_text AUTO_INCREMENT=1;
         ALTER TABLE customer_entity_varchar AUTO_INCREMENT=1;
-        ALTER TABLE log_customer AUTO_INCREMENT=1;
-        ALTER TABLE log_visitor AUTO_INCREMENT=1;
-        ALTER TABLE log_visitor_info AUTO_INCREMENT=1;
+        ALTER TABLE login_as_customer AUTO_INCREMENT=1;
+        ALTER TABLE customer_visitor AUTO_INCREMENT=1;
         SET FOREIGN_KEY_CHECKS=1;
         `);
     }
