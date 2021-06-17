725a914615df6c6a64a47314c0fbd005d4359d72
ejnshtein <ejnshtein@gmail.com>
2021-06-17 15:30:29 +0300

    if not remoteDb option was suplied, ask for connection string for remote-db

--- build-packages/magento-scripts/lib/tasks/mysql/import-remote-db/index.js
+++ build-packages/magento-scripts/lib/tasks/mysql/import-remote-db/index.js
@@ -6,9 +6,18 @@ const sshDb = require('./ssh');
  * @type {import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
  */
 const importRemoteDbSSH = {
-    skip: (ctx) => !ctx.remoteDb,
+    skip: (ctx) => typeof ctx.remoteDb === 'undefined',
     task: async (ctx, task) => {
         task.title = 'Importing database from remote server';
+
+        if (!ctx.remoteDb) {
+            ctx.remoteDb = await task.prompt({
+                type: 'Input',
+                message: `Please enter a remote server connection string.
+It can be a SSH (ssh://<url>) connection or mysql (mysql://<url>) connection.
+`
+            });
+        }
 
         const url = new URL(ctx.remoteDb);
 
