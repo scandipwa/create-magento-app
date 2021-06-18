3d7ca004b37b81ab0955443dd2f015b4475a2520
ejnshtein <ejnshtein@gmail.com>
2021-06-18 11:01:17 +0300

    print connect mysql errors

--- build-packages/magento-scripts/lib/tasks/mysql/connect-to-mysql.js
+++ build-packages/magento-scripts/lib/tasks/mysql/connect-to-mysql.js
@@ -12,9 +12,10 @@ const connectToMySQL = {
         const { config: { docker }, ports } = ctx;
         const { mysql: { env } } = docker.getContainers();
         let tries = 0;
-        while (tries < 10) {
+        const errors = [];
+        while (tries < 20) {
             tries++;
-            if (tries === 3) {
+            if (tries === 10) {
                 task.output = 'Still connecting, do not worry.\nProbably MySQL server is still starting...';
             }
             try {
@@ -29,12 +30,13 @@ const connectToMySQL = {
                 ctx.mysqlConnection = connection;
                 break;
             } catch (e) {
+                errors.push(e);
                 //
             }
-            await sleep(5000);
+            await sleep(1000);
         }
         if (tries === 10) {
-            throw new Error('Unable to connect to MySQL server. Check your server configuration!');
+            throw new Error(`Unable to connect to MySQL server. Check your server configuration!\n\n${ errors.join(' ') }`);
         }
 
         task.title = `MySQL server connected${tries > 2 ? ` after ${tries} tries.` : '!'}`;
