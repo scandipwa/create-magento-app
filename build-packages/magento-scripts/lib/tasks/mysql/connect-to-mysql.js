c7e5896c691550b4b547be7ce20b001ebdae46bc
ejnshtein <ejnshtein@gmail.com>
2021-06-22 14:01:03 +0300

    handle mysql initialization correctly and extend connection time

--- build-packages/magento-scripts/lib/tasks/mysql/connect-to-mysql.js
+++ build-packages/magento-scripts/lib/tasks/mysql/connect-to-mysql.js
@@ -1,22 +1,31 @@
 /* eslint-disable no-await-in-loop,no-param-reassign */
 const mysql = require('mysql2/promise');
+const { execAsyncSpawn } = require('../../util/exec-async-command');
 const sleep = require('../../util/sleep');
 
 /**
  * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
  */
 const connectToMySQL = {
-    title: 'Connecting to MySQL server',
+    title: 'Connecting to MySQL server...',
     task: async (ctx, task) => {
-        task.title = 'Connecting to MySQL server...';
         const { config: { docker }, ports } = ctx;
-        const { mysql: { env } } = docker.getContainers();
+        const { mysql: { env, name } } = docker.getContainers();
         let tries = 0;
+        let maxTries = 20;
         const errors = [];
-        while (tries < 20) {
+        while (tries < maxTries) {
             tries++;
-            if (tries === 10) {
-                task.output = 'Still connecting, do not worry.\nProbably MySQL server is still starting...';
+
+            if (maxTries !== 120) {
+                const mysqlOutput = await execAsyncSpawn(`docker logs ${name}`);
+                if (mysqlOutput.includes('Initializing database files')) {
+                    maxTries = 120;
+                    task.output = `MySQL is initializing database files!
+This will take some time on the first time.
+Please wait and do not restart the MySQL container until initialization is finished!`;
+                }
+                await sleep(2000);
             }
             try {
                 const connection = await mysql.createConnection({
@@ -31,15 +40,15 @@ const connectToMySQL = {
                 break;
             } catch (e) {
                 errors.push(e);
-                //
             }
             await sleep(1000);
         }
-        if (tries === 10) {
+
+        if (tries === maxTries) {
             throw new Error(`Unable to connect to MySQL server. Check your server configuration!\n\n${ errors.join(' ') }`);
         }
 
-        task.title = `MySQL server connected${tries > 2 ? ` after ${tries} tries.` : '!'}`;
+        task.title = 'MySQL server connected!';
     },
     options: {
         bottomBar: 10
