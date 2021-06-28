f8653cc91ed196137ea271103b452ec60de04867
ejnshtein <ejnshtein@gmail.com>
2021-06-28 18:57:01 +0300

    handle if 2fa module is not available (if running on magento 2.3)

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/disable-2fa.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/disable-2fa.js
@@ -6,10 +6,15 @@ const runMagentoCommand = require('../../../util/run-magento');
 module.exports = {
     title: 'Disabling 2fa for admin.',
     task: async ({ magentoVersion }, task) => {
-        const { result } = await runMagentoCommand('module:status Magento_TwoFactorAuth', {
+        const { result, code } = await runMagentoCommand('module:status Magento_TwoFactorAuth', {
             magentoVersion,
             throwNonZeroCode: false
         });
+
+        if (code !== 0) {
+            task.skip();
+            return;
+        }
 
         if (result.includes('Module is disabled')) {
             task.skip();
