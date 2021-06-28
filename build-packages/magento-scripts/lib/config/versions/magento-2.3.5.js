d540ba6be3c6ef661565c207f061f8f877716bdc
ejnshtein <ejnshtein@gmail.com>
2021-06-28 18:57:36 +0300

    add magento 2.3.5 configuration

---
+++ build-packages/magento-scripts/lib/config/versions/magento-2.3.5.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { defaultMagentoConfig } = require('../magento-config');
+
+module.exports = ({ templateDir } = {}) => ({
+    magentoVersion: '2.3.5',
+    configuration: {
+        php: {
+            version: '7.3.28',
+            configTemplate: path.join(templateDir || '', 'php.template.ini'),
+            extensions: {
+                gd: {},
+                intl: {},
+                zlib: {},
+                openssl: {},
+                sockets: {},
+                SimpleXML: {},
+                xdebug: {
+                    version: '3.0.4'
+                }
+            }
+        },
+        nginx: {
+            version: '1.18.0',
+            configTemplate: path.join(templateDir || '', 'nginx.template.conf')
+        },
+        redis: {
+            version: '5.0'
+        },
+        mysql: {
+            version: '5.7'
+        },
+        mariadb: {
+            version: '10.2'
+        },
+        elasticsearch: {
+            version: '7.6.2'
+        },
+        composer: {
+            version: '1'
+        }
+    },
+    magento: defaultMagentoConfig,
+    host: 'localhost',
+    ssl: {
+        enabled: false
+    }
+});
