605e2e590183604703668aa0fd46b36c0d422286
ejnshtein <ejnshtein@gmail.com>
2021-07-02 15:07:56 +0300

    WIP install phpbrew works on macos

--- build-packages/magento-scripts/lib/tasks/requirements/install-phpbrew.js
+++ build-packages/magento-scripts/lib/tasks/requirements/install-phpbrew.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable consistent-return */
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
-const osPlatform = require('../../../util/os-platform');
-const { execCommandTask } = require('../../util/exec-async-command');
+const osPlatform = require('../../util/os-platform');
+const { execCommandTask, execAsyncSpawn } = require('../../util/exec-async-command');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const dependenciesForPlatforms = require('../../config/dependencies-for-platforms');
 
@@ -55,27 +56,101 @@ const installPHPDependency = {
 /**
  * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
  */
-const installPhpbrew = {
+const installXcode = {
+    title: 'Installing XCode',
+    task: async (ctx, task) => {
+        const { code: XCODEInstallCode } = await execAsyncSpawn('xcode-select -p 1>/dev/null;echo $?', {
+            throwNonZeroCode: false,
+            withCode: true
+        });
+
+        if (XCODEInstallCode !== 0) {
+            return task.newListr({
+                task: async (subCtx, subTask) => {
+                    await execAsyncSpawn('xcode-select --install', {
+                        pipeInput: true,
+                        callback: (t) => {
+                            subTask.output = t;
+                        }
+                    });
+                },
+                options: {
+                    bottomBar: 10
+                }
+            });
+        }
+
+        task.skip();
+    }
+};
+
+/**
+ * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
+ */
+const installPHPBrewDependencies = {
+    title: 'Installing PHPBrew dependencies',
+    task: async (ctx, task) => {
+        const installedDependencies = (await execAsyncSpawn('brew list')).split('\n');
+
+        if (['autoconf', 'pkg-config'].every((dep) => installedDependencies.includes(dep))) {
+            task.skip();
+            return;
+        }
+
+        return task.newListr(
+            execCommandTask('brew install autoconf pkg-config', {
+                pipeInput: true
+            })
+        );
+    }
+};
+
+/**
+ * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
+ */
+const installPHPBrewBinary = {
+    title: 'Installing PHPBrew binary',
+    task: async (ctx, task) => {
+        task.output = 'Downloading PHPbrew binary...';
+        await execAsyncSpawn('curl -L -O https://github.com/phpbrew/phpbrew/releases/latest/download/phpbrew.phar', {
+            throwNonZeroCode: true
+        });
+
+        task.output = 'Making PHPbrew binary executable...';
+        await execAsyncSpawn('chmod +x phpbrew.phar', {
+            throwNonZeroCode: true
+        });
+
+        task.output = 'Moving PHPbrew binary to /url/local/bin directory...';
+        task.output = 'Enter your sudo password:';
+        await execAsyncSpawn('sudo mv phpbrew.phar /usr/local/bin/phpbrew', {
+            throwNonZeroCode: true,
+            pipeInput: true
+        });
+
+        task.output = 'Initializing PHPbrew...';
+        await execAsyncSpawn('phpbrew init', {
+            throwNonZeroCode: true
+        });
+    },
+    options: {
+        bottomBar: 10
+    }
+};
+
+/**
+ * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
+ */
+const installPHPBrew = {
     title: 'Installing PHPBrew',
     task: async (ctx, task) => {
         const currentPlatform = os.platform();
 
         if (currentPlatform === 'darwin') {
             return task.newListr([
-                execCommandTask('xcode-select --install', {
-                    pipeInput: true
-                }),
-                execCommandTask('brew install autoconf pkg-config', {
-                    pipeInput: true
-                }),
-                execCommandTask('curl -L -O https://github.com/phpbrew/phpbrew/releases/latest/download/phpbrew.phar'),
-                execCommandTask('chmod +x phpbrew.phar', {
-                    pipeInput: true
-                }),
-                execCommandTask('sudo mv phpbrew.phar /usr/local/bin/phpbrew', {
-                    pipeInput: true
-                }),
-                execCommandTask('phpbrew init'),
+                installXcode,
+                installPHPBrewDependencies,
+                installPHPBrewBinary,
                 {
                     task: async (subCtx, subTask) => {
                         const addLineToShellConfigFIle = await subTask.prompt({
@@ -93,12 +168,12 @@ Then you can continue installation.`);
                         if (process.env.SHELL.includes('zsh')) {
                             await fs.promises.appendFile(
                                 path.join(os.homedir(), '.zshrc'),
-                                'source ~/.phpbrew/bashrc'
+                                '\nsource ~/.phpbrew/bashrc\n'
                             );
                         } else if (process.env.SHELL.includes('bash')) {
                             await fs.promises.appendFile(
                                 path.join(os.homedir(), '.zshrc'),
-                                'source ~/.phpbrew/bashrc'
+                                '\nsource ~/.phpbrew/bashrc\n'
                             );
                         } else {
                             const continueInstall = await subTask.prompt({
@@ -134,4 +209,4 @@ Then you can continue installation.`);
     }
 };
 
-module.exports = installPhpbrew;
+module.exports = installPHPBrew;
