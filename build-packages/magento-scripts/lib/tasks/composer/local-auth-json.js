2e34fc73f207b9b305fa8fd9750760d72602ffc1
ejnshtein <ejnshtein@gmail.com>
2021-07-07 15:07:43 +0300

    add checks for local auth.json

--- build-packages/magento-scripts/lib/tasks/composer/local-auth-json.js
+++ build-packages/magento-scripts/lib/tasks/composer/local-auth-json.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const fs = require('fs');
 const pathExists = require('../../util/path-exists');
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 
 /**
  * @type {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
@@ -10,7 +11,20 @@ const localAuthJson = {
     task: async (ctx, task) => {
         if (await pathExists(path.join(process.cwd(), 'auth.json'))) {
             task.title = 'Using local auth.json';
-            process.env.COMPOSER_AUTH = await fs.promises.readFile(path.join(process.cwd(), 'auth.json'));
+            const localAuthJson = await fs.promises.readFile(path.join(process.cwd(), 'auth.json'));
+
+            let localAuthJsonContent;
+            try {
+                localAuthJsonContent = JSON.parse(localAuthJson);
+            } catch (e) {
+                throw new Error(`Could not parse ./auth.json file as JSON!\n\n${e}`);
+            }
+
+            if (!localAuthJsonContent || !localAuthJsonContent['http-basic'] || !localAuthJsonContent['http-basic']['repo.magento.com']) {
+                throw new Error(`Your ./auth.json file does not contain the ${ logger.style.misc("{ 'http-basic': { 'repo.magento.com': <> } }") } field.`);
+            }
+
+            process.env.COMPOSER_AUTH = localAuthJson;
         }
     },
     options: {
