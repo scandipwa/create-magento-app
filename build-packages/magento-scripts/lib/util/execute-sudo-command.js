7ee76a4902acffc156885c72e2ccecb1c4b1d46a
ejnshtein <ejnshtein@gmail.com>
2021-07-08 15:51:24 +0300

    add options to execute sudo command

--- build-packages/magento-scripts/lib/util/execute-sudo-command.js
+++ build-packages/magento-scripts/lib/util/execute-sudo-command.js
@@ -1,12 +1,13 @@
+/* eslint-disable max-len */
 /* eslint-disable no-param-reassign */
 const os = require('os');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const { execCommandTask } = require('./exec-async-command');
 
 /**
- * @type {(command: string) => import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
+ * @type {(command: string, options: Omit<import('../util/exec-async-command').ExecAsyncSpawnOptions<false>, 'callback' | 'pipeInput'>) => import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
  */
-const executeSudoCommand = (command) => ({
+const executeSudoCommand = (command, options = {}) => ({
     task: async (ctx, task) => {
         task.output = 'Enter your sudo password!';
         task.output = logger.style.command(`>[sudo] password for ${ os.userInfo().username }:`);
@@ -16,7 +17,8 @@ const executeSudoCommand = (command) => ({
                 callback: (t) => {
                     task.output = t;
                 },
-                pipeInput: true
+                pipeInput: true,
+                ...options
             })
         ]);
     },
