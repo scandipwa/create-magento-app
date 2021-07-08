08ab3d728675c27b5408ae4869e117f25051f11c
ejnshtein <ejnshtein@gmail.com>
2021-07-08 17:06:41 +0300

    update get-project-configuration

--- build-packages/magento-scripts/lib/config/get-project-configuration.js
+++ build-packages/magento-scripts/lib/config/get-project-configuration.js
@@ -1,21 +1,15 @@
 /* eslint-disable no-param-reassign */
 const { getConfigFromMagentoVersion } = require('./index');
-const { folderName } = require('../util/prefix');
-const convertLegacyVolumes = require('../tasks/docker/convert-legacy-volumes');
 
 /**
  * @type {import('listr2').ListrTask<import('../../typings/context').ListrContext>}
  */
 const getProjectConfiguration = {
     title: 'Getting project configuration',
-    task: async (ctx, task) => {
+    task: async (ctx) => {
         const { magentoVersion } = ctx;
 
-        ctx.config = await getConfigFromMagentoVersion(magentoVersion, process.cwd(), folderName);
-
-        return task.newListr(
-            convertLegacyVolumes
-        );
+        ctx.config = await getConfigFromMagentoVersion(magentoVersion, process.cwd());
     }
 };
 
