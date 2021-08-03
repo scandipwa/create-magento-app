1741bfcb178ae2c29f30b69f8a942437e724f76e
Okabe Rintaro <ejnshtein@gmail.com>
2021-08-03 13:59:57 +0300

    refactor disable full page cache

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/disable-page-cache.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/disable-page-cache.js
@@ -1,26 +1,11 @@
-/* eslint-disable no-param-reassign */
-const runMagentoCommand = require('../../../util/run-magento');
+const magentoTask = require('../../../util/magento-task');
 
 /**
  * @type {import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
  */
 const disablePageCache = {
     title: 'Disabling full_page cache in Magento',
-    task: async ({ magentoVersion }, task) => {
-        try {
-            await runMagentoCommand('cache:disable full_page', {
-                magentoVersion,
-                callback: (t) => {
-                    task.output = t;
-                }
-            });
-        } catch (e) {
-            throw new Error(
-                `Unexpected error while disabling full page cache.
-                See ERROR log below.\n\n${e}`
-            );
-        }
-    }
+    task: (ctx, task) => task.newListr(magentoTask('cache:disable full_page'))
 };
 
 module.exports = disablePageCache;
