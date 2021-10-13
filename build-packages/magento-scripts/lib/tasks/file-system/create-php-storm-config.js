20892dc48f9e29875cef1915c1a4e5aa5035a182
arthur.morais <arthur.morais@scandiweb.com>
2021-10-13 17:40:04 -0300

    CTO-81 added PHPStorm configuration for database feature

--- build-packages/magento-scripts/lib/tasks/file-system/create-php-storm-config.js
+++ build-packages/magento-scripts/lib/tasks/file-system/create-php-storm-config.js
@@ -11,7 +11,12 @@ const createPhpStormConfig = () => ({
         const { serverName } = config.xdebug;
         const { runManagerName } = config.xdebug;
         const { sessionId } = config.xdebug;
+        const databaseDriver = config.database.driver;
+
         const phpVersion = config.php.version;
+
+        const { dataSourceManagerName } = config.database;
+        const { jdbcUrl } = config.database;
         try {
             await setConfigFile({
                 configPathname: config.xdebug.path,
@@ -23,7 +28,8 @@ const createPhpStormConfig = () => ({
                     debugServerAddress,
                     serverName,
                     runManagerName,
-                    sessionId
+                    sessionId,
+                    databaseDriver
                 }
             });
         } catch (e) {
@@ -41,6 +47,33 @@ const createPhpStormConfig = () => ({
             });
         } catch (e) {
             throw new Error(`Unexpected error accrued during php.xml config creation\n\n${e}`);
+        }
+
+        try {
+            await setConfigFile({
+                configPathname: config.database.dataSourcesLocal.path,
+                template: config.database.dataSourcesLocal.templatePath,
+                overwrite: true,
+                templateArgs: {
+                    dataSourceManagerName
+                }
+            });
+        } catch (e) {
+            throw new Error(`Unexpected error accrued during dataSources.local.xml config creation\n\n${e}`);
+        }
+
+        try {
+            await setConfigFile({
+                configPathname: config.database.dataSources.path,
+                template: config.database.dataSources.templatePath,
+                overwrite: true,
+                templateArgs: {
+                    dataSourceManagerName,
+                    jdbcUrl
+                }
+            });
+        } catch (e) {
+            throw new Error(`Unexpected error accrued during dataSources.xml config creation\n\n${e}`);
         }
     }
 });
