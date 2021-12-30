3df3b990c0e9c1414c02fa78fb5d370292114f8a
Okabe Rintaro <ejnshtein@gmail.com>
2021-12-30 15:52:05 +0200

    add dependency check to create-magento-app

---
+++ build-packages/create-magento-app/lib/dependency/install-dependencies.js
@@ -0,0 +1,57 @@
+// const os = require('os');
+const { prompt } = require('enquirer');
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
+const execAsync = require('../exec-async');
+const dependenciesForPlatforms = require('./dependencies-for-platforms');
+
+/**
+ * Install dependencies
+ * @param {object} options
+ * @param {keyof dependenciesForPlatforms} options.platform Platform
+ * @param {string[]} options.dependenciesToInstall List of dependencies to install
+ */
+const installDependencies = async (options) => {
+    const { dependenciesToInstall, platform } = options;
+    const cmd = dependenciesForPlatforms[platform].installCommand(dependenciesToInstall.join(' '));
+    const installCommand = logger.style.code(cmd);
+    const dependenciesWordFormatter = `dependenc${dependenciesToInstall.length > 1 ? 'ies' : 'y'}`;
+    logger.logN(`Missing ${ dependenciesWordFormatter } ${ logger.style.code(dependenciesToInstall.join(', ')) } detected!`);
+    logger.logN('These dependencies required for CMA to operate!');
+
+    const { installAnswer } = await prompt({
+        type: 'select',
+        message: `Do you want to install missing ${ dependenciesWordFormatter } now?`,
+        name: 'installAnswer',
+        choices: [
+            {
+                name: 'install',
+                message: `Install ${ dependenciesWordFormatter } now!`
+            },
+            {
+                name: 'not-install',
+                message: `Install ${ dependenciesWordFormatter } later, when I feel it.`
+            }
+        ]
+    });
+
+    if (installAnswer === 'not-install') {
+        throw new Error(`Okay, skipping ${ dependenciesWordFormatter } installation for now.
+
+To install missing ${ dependenciesWordFormatter } manually, run the following command: ${ installCommand }`);
+    }
+
+    if (installAnswer === 'install') {
+        // on macos we don't need sudo permissions to install dependencies, so every other platform required to do that
+        // if (platform !== 'darwin') {
+        //     logger.logN(`Enter your sudo password! It's needed for ${ dependenciesWordFormatter } installation.`);
+        //     logger.logN(logger.style.command(`>[sudo] password for ${ os.userInfo().username }:`));
+        // }
+
+        await execAsync(cmd, {
+            callback: logger.log,
+            pipeInput: true
+        });
+    }
+};
+
+module.exports = installDependencies;
---
+++ build-packages/create-magento-app/lib/dependency/mac.js
@@ -0,0 +1,21 @@
+const dependenciesForPlatforms = require('./dependencies-for-platforms');
+const execAsync = require('../exec-async');
+const installDependencies = require('./install-dependencies');
+
+const macDependenciesCheck = async () => {
+    const installedDependencies = (await execAsync('brew list')).split('\n');
+
+    const dependenciesToInstall = dependenciesForPlatforms
+        .darwin
+        .dependencies
+        .filter((dep) => !installedDependencies.includes(dep));
+
+    if (dependenciesToInstall.length > 0) {
+        return installDependencies({
+            platform: 'darwin',
+            dependenciesToInstall
+        });
+    }
+};
+
+module.exports = macDependenciesCheck;
