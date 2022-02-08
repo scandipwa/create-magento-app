a6ae24bfb4d97072860d47671802827a48bb6f7f
Okabe Rintaro <ejnshtein@gmail.com>
2022-02-08 16:29:40 +0200

    run apt update before installing missing dependencies on ubuntu

--- build-packages/magento-scripts/lib/tasks/requirements/dependency/ubuntu.js
+++ build-packages/magento-scripts/lib/tasks/requirements/dependency/ubuntu.js
@@ -1,8 +1,30 @@
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
+const os = require('os');
 const dependenciesForPlatforms = require('../../../config/dependencies-for-platforms');
-const { execAsyncSpawn } = require('../../../util/exec-async-command');
+const { execAsyncSpawn, execCommandTask } = require('../../../util/exec-async-command');
 const installDependenciesTask = require('../../../util/install-dependencies-task');
 
 const pkgRegex = /^(\S+)\/\S+\s(\S+)\s\S+\s\S+$/i;
+
+const updateSystem = () => ({
+    title: 'Updating your system',
+    task: async (ctx, task) => {
+        task.output = 'Enter your sudo password!';
+        task.output = logger.style.command(`>[sudo] password for ${ os.userInfo().username }:`);
+
+        return task.newListr(
+            execCommandTask('sudo apt update', {
+                callback: (t) => {
+                    task.output = t;
+                },
+                pipeInput: true
+            })
+        );
+    },
+    options: {
+        bottomBar: 10
+    }
+});
 
 /**
  * @type {() => import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
@@ -28,6 +50,23 @@ const ubuntuDependenciesCheck = () => ({
             .map((dep) => (Array.isArray(dep) ? dep[0] : dep));
 
         if (dependenciesToInstall.length > 0) {
+            const runSystemUpdate = await task.prompt({
+                type: 'Confirm',
+                message: `Would you like to run ${logger.style.command('apt update')} before installing missing ${dependenciesToInstall.length} dependenc${dependenciesToInstall.length > 1 ? 'ies' : 'y'}?`
+            });
+
+            if (runSystemUpdate) {
+                return task.newListr([
+                    updateSystem(),
+                    installDependenciesTask({
+                        platform: 'Ubuntu',
+                        dependenciesToInstall
+                    })
+                ], {
+                    concurrent: false
+                });
+            }
+
             return task.newListr(
                 installDependenciesTask({
                     platform: 'Ubuntu',
