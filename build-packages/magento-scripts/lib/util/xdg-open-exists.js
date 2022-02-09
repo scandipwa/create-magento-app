0ade0ca46af67c7182df0b6d57809c078c26abae
Okabe Rintaro <ejnshtein@gmail.com>
2022-02-09 17:42:38 +0200

    check for xdg-open in path on WSL system

---
+++ build-packages/magento-scripts/lib/util/xdg-open-exists.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const pathExists = require('./path-exists');
+
+const checkForXDGOpen = async () => {
+    const pathParts = process.env.PATH.split(':');
+
+    const results = await Promise.all(
+        pathParts.map(
+            async (pathPart) => {
+                if (!await pathExists(pathPart)) {
+                    return false;
+                }
+
+                const files = await fs.promises.readdir(pathPart, {
+                    encoding: 'utf-8',
+                    withFileTypes: true
+                });
+
+                return files.some((file) => file.isFile() && file.name === 'open');
+            }
+        )
+    );
+
+    return results.includes(true);
+};
+
+module.exports = checkForXDGOpen;
