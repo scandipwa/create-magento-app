ae55df78d64e6d681d9f24231dfe4cf16ef4ef5c
Okabe Rintaro <ejnshtein@gmail.com>
2022-04-12 19:22:15 +0300

    add magento 2.4.4, 2.4.3-p2, 2.3.7-p3 from april 12 release, add missing magento 2.3.7-p2

---
+++ build-packages/magento-scripts/lib/config/versions/magento-2.3.7-p3.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { defaultMagentoConfig } = require('../magento-config');
+
+module.exports = ({ templateDir } = {}) => ({
+    magentoVersion: '2.3.7-p3',
+    configuration: {
+        php: {
+            version: '7.4.27',
+            configTemplate: path.join(templateDir || '', 'php.template.ini'),
+            extensions: {
+                gd: {},
+                intl: {},
+                zlib: {},
+                openssl: {},
+                sockets: {},
+                SimpleXML: {},
+                xdebug: {
+                    version: '3.1.2'
+                }
+            }
+        },
+        nginx: {
+            version: '1.18.0',
+            configTemplate: path.join(templateDir || '', 'nginx.template.conf')
+        },
+        redis: {
+            version: '6'
+        },
+        mysql: {
+            version: '5.7'
+        },
+        mariadb: {
+            version: '10.2'
+        },
+        elasticsearch: {
+            version: '7.9.3'
+        },
+        composer: {
+            version: '2'
+        }
+    },
+    magento: defaultMagentoConfig,
+    host: 'localhost',
+    ssl: {
+        enabled: false
+    }
+});
