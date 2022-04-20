75937e15178565e887d2d8af2c813a2a3660ebfa
Okabe Rintaro <ejnshtein@gmail.com>
2022-04-20 17:50:07 +0300

    stage inspection tools config refactoring

--- build-packages/magento-scripts/lib/tasks/file-system/create-phpstorm-config/inspection-tools-config.js
+++ build-packages/magento-scripts/lib/tasks/file-system/create-phpstorm-config/inspection-tools-config.js
@@ -1,4 +1,28 @@
-const setConfigFile = require('../../../util/set-config');
+const { loadXmlFile } = require('../../../config/xml-parser');
+const pathExists = require('../../../util/path-exists');
+// const setConfigFile = require('../../../util/set-config');
+
+const setupPhpCSFixer = (inspectionToolsData) => {
+    const phpCSFixerConfig = inspectionToolsData.find((inspectionToolData) => inspectionToolData['@_class'] === 'PhpCSFixerValidationInspection');
+    if (!phpCSFixerConfig) {
+        inspectionToolsData.push({
+            option: [
+                {
+                    '@_name': 'CODING_STANDARD',
+                    '@_value': 'Custom'
+                },
+                {
+                    '@_name': 'CUSTOM_RULESET_PATH',
+                    '@_value': '$PROJECT_DIR$/.php_cs.dist'
+                }
+            ],
+            '@_class': 'PhpCSFixerValidationInspection',
+            '@_enabled': 'true',
+            '@_level': 'ERROR',
+            '@_enabled_by_default': 'true'
+        });
+    }
+};
 
 /**
  * @type {() => import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
@@ -8,20 +32,28 @@ const setupInspectionToolsConfig = () => ({
     task: async (ctx) => {
         const {
             config: {
-                phpStorm
+                phpStorm: {
+                    inspectionTools
+                }
             }
         } = ctx;
 
-        try {
-            await setConfigFile({
-                configPathname: phpStorm.inspectionTools.path,
-                template: phpStorm.inspectionTools.templatePath,
-                overwrite: true,
-                templateArgs: {}
-            });
-        } catch (e) {
-            throw new Error(`Unexpected error accrued during Project_Default.xml config creation\n\n${e}`);
+        if (await pathExists(inspectionTools.path)) {
+            const inspectionToolsData = await loadXmlFile(inspectionTools.path);
+            setupPhpCSFixer(inspectionToolsData.component.profile.inspection_tool);
+            console.log(inspectionToolsData);
         }
+
+        // try {
+        //     await setConfigFile({
+        //         configPathname: phpStorm.inspectionTools.path,
+        //         template: phpStorm.inspectionTools.templatePath,
+        //         overwrite: true,
+        //         templateArgs: {}
+        //     });
+        // } catch (e) {
+        //     throw new Error(`Unexpected error accrued during Project_Default.xml config creation\n\n${e}`);
+        // }
     }
 });
 
