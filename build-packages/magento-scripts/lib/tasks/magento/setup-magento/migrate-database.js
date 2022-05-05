fbbd06c12220f4a9bb0d5a87ce507636dd69e3de
Okabe Rintaro <ejnshtein@gmail.com>
2022-05-05 17:51:05 +0300

    add varnish setup to more migrade-db tasks, fix import issue for mysql fix-db task

--- build-packages/magento-scripts/lib/tasks/magento/setup-magento/migrate-database.js
+++ build-packages/magento-scripts/lib/tasks/magento/setup-magento/migrate-database.js
@@ -72,6 +72,7 @@ const migrateDatabase = (options = {}) => ({
                 upgradeMagento(),
                 magentoTask('cache:enable'),
                 configureElasticsearch()
+                varnishConfigSetup(),
             ], {
                 concurrent: false,
                 exitOnError: true,
