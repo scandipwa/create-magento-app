f5f3a23e040884eee848187b87232a63c62a1d3c
Okabe Rintaro <ejnshtein@gmail.com>
2022-05-23 18:37:55 +0300

    refactor analytics util to be more readable

--- build-packages/magento-scripts/lib/util/analytics.js
+++ build-packages/magento-scripts/lib/util/analytics.js
@@ -1,15 +1,137 @@
 const os = require('os');
 const { request } = require('smol-request');
-const { getSystemConfigSync } = require('../config/system-config');
-const { getExternalIpAddress } = require('./ip');
 const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const generateUUID = require('@tilework/mosaic-dev-utils/uuid');
-// const { getSystemConfig } = require('./get-configuration-file');
+const { getSystemConfigSync } = require('../config/system-config');
+const { getExternalIpAddress } = require('./ip');
+const pkg = require('../../package.json');
 
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID || 'UA-127741417-8';
 const UNKNOWN = 'unknown';
 
 const anonymizeError = (text) => text.replace(new RegExp(`${os.homedir()}`, 'gi'), '/home/magento-scripts-user');
+
+/**
+ * Google Analytics Parameters
+ * @typedef {Object} GAParameters
+ * @property {String} version
+ * @property {String} trackingID
+ * @property {String} [dataSource]
+ * @property {Object} [user]
+ * @property {String} [user.clientID]
+ * @property {String} [user.userID]
+ * @property {Object} [session]
+ * @property {String} [session.sessionControl]
+ * @property {String} [session.ipOverride]
+ * @property {String} [session.userAgentOverride]
+ * @property {Object} [systemInfo]
+ * @property {String} [systemInfo.userLanguage]
+ * @property {Object} [contentInformation]
+ * @property {String} [contentInformation.documentLocationUrl]
+ * @property {String} [contentInformation.documentPath]
+ * @property {String} [contentInformation.documentHostName]
+ * @property {Object} [timing]
+ * @property {String} [timing.userTimingCategory]
+ * @property {String} [timing.userTimingVariableName]
+ * @property {String} [timing.userTimingTime]
+ * @property {String} [timing.userTimingLabel]
+ * @property {Object} [hit]
+ * @property {String} [hit.type]
+ * @property {Object} [exception]
+ * @property {String} [exception.description]
+ * @property {Boolean} [exception.fatal]
+ * @property {Object} [event]
+ * @property {String} event.category
+ * @property {String} event.action
+ * @property {String} [event.label]
+ * @property {String} [event.value]
+ * @property {Object} [app]
+ * @property {String} [app.name]
+ * @property {String} [app.id]
+ * @property {String} [app.version]
+ * @property {String} [app.installedID]
+ * @property {Record<string, string>} [customDimension]
+ */
+
+/**
+ * @type {GAParameters}
+ */
+const gaParametersMapping = {
+    version: 'v',
+    trackingID: 'tid',
+    user: {
+        clientID: 'cid',
+        userID: 'uid'
+    },
+    session: {
+        sessionControl: 'sc',
+        ipOverride: 'uip',
+        userAgentOverride: 'ua'
+    },
+    systemInfo: {
+        userLanguage: 'ul'
+    },
+    contentInformation: {
+        documentLocationUrl: 'dl',
+        documentPath: 'dp',
+        documentHostName: 'dh'
+    },
+    timing: {
+        userTimingCategory: 'utc',
+        userTimingVariableName: 'utv',
+        userTimingTime: 'utt',
+        userTimingLabel: 'utl'
+    },
+    hit: {
+        type: 't'
+    },
+    exception: {
+        description: 'exd',
+        fatal: 'exf'
+    },
+    event: {
+        category: 'ec',
+        action: 'ea',
+        label: 'el',
+        value: 'ev'
+    },
+    app: {
+        name: 'an',
+        id: 'aid',
+        version: 'av',
+        installedID: 'aiid'
+    },
+    customDimension: Array.from({ length: 200 }, (_, i) => i + 1).reduce((acc, val) => ({
+        ...acc,
+        [`customDimension${val}`]: `cd${val}`
+    }), {})
+};
+
+const getAppData = () => ({
+    name: pkg.name,
+    version: pkg.version
+});
+
+/**
+ * @param {GAParameters} parameters
+ */
+const collectAnalyticsParameters = (parameters) => {
+    const parsedParameters = {};
+
+    for (const [firstLevelKey, firstLevelValue] of Object.entries(gaParametersMapping)) {
+        if (typeof firstLevelValue === 'object' && firstLevelValue !== null) {
+            for (const [secondLevelKey, secondLevelValue] of Object.entries(firstLevelValue)) {
+                if (parameters[firstLevelKey] && parameters[firstLevelKey][secondLevelKey]) {
+                    parsedParameters[secondLevelValue] = parameters[firstLevelKey][secondLevelKey];
+                }
+            }
+        } else if (parameters[firstLevelKey]) {
+            parsedParameters[firstLevelValue] = parameters[firstLevelKey];
+        }
+    }
+
+    return parsedParameters;
+};
 
 class Analytics {
     constructor() {
@@ -52,21 +174,32 @@ class Analytics {
         return !analytics;
     }
 
+    /**
+     * @param {GAParameters} data
+     */
     async _collect(data) {
         if (this.isGaDisabled) {
             // skip GA
             return;
         }
 
-        const rawBody = {
+        /**
+         * @type {GAParameters}
+         */
+        const analyticsParameters = {
             ...data,
-            v: '1',
-            tid: this.gaTrackingId,
-            cid: this.clientIdentifier
+            version: '1',
+            trackingID: this.gaTrackingId,
+            user: {
+                clientID: this.clientIdentifier
+            },
+            app: getAppData()
         };
 
         try {
-            rawBody.uip = await getExternalIpAddress();
+            analyticsParameters.systemInfo = {
+                ipOverride: await getExternalIpAddress()
+            };
         } catch (e) {
             console.log(e);
             // Do nothing
@@ -74,7 +207,9 @@ class Analytics {
 
         if (this.lang !== UNKNOWN) {
             // get system language here
-            rawBody.ul = this.lang;
+            analyticsParameters.systemInfo = {
+                userLanguage: this.lang
+            };
         }
 
         if (this.currentUrl !== UNKNOWN) {
@@ -83,12 +218,14 @@ class Analytics {
                 pathname
             } = new URL(this.currentUrl);
 
-            rawBody.dp = pathname;
-            rawBody.dh = hostname;
-            rawBody.dl = this.currentUrl;
+            analyticsParameters.contentInformation = {
+                documentLocationUrl: this.currentUrl,
+                documentPath: pathname,
+                documentHostName: hostname
+            };
         }
 
-        const params = new URLSearchParams(rawBody).toString();
+        const params = new URLSearchParams(collectAnalyticsParameters(analyticsParameters)).toString();
 
         try {
             if (!process.env.GA_DEBUG) {
@@ -111,7 +248,7 @@ class Analytics {
                 }
             );
 
-            logger.log(rawBody, jsonResponse);
+            logger.log(analyticsParameters, jsonResponse);
             logger.log(jsonResponse.hitParsingResult[0].parserMessage);
 
         // eslint-disable-next-line no-empty
@@ -123,36 +260,69 @@ class Analytics {
     trackError(error, isFatal = true) {
         // return; // nothing
         return this._collect({
-            t: 'exception',
-            exd: anonymizeError(typeof error === 'string' ? error : error.message),
-            exf: isFatal
+            hit: {
+                type: 'exception'
+            },
+            exception: {
+                description: anonymizeError(typeof error === 'string' ? error : error.message),
+                fatal: isFatal
+            }
         });
+        // return this._collect({
+        //     t: 'exception',
+        //     exd: anonymizeError(typeof error === 'string' ? error : error.message),
+        //     exf: isFatal
+        // });
     }
 
     trackTiming(label, time, category = UNKNOWN) {
         return this._collect({
-            t: 'timing',
-            utc: category,
-            utv: label,
-            utl: this.currentUrl,
-            utt: Math.round(time)
+            hit: {
+                type: 'timing'
+            },
+            timing: {
+                userTimingCategory: category,
+                userTimingVariableName: label,
+                userTimingLabel: this.currentUrl,
+                userTimingTime: Math.round(time)
+            }
         });
+        // return this._collect({
+        //     t: 'timing',
+        //     utc: category,
+        //     utv: label,
+        //     utl: this.currentUrl,
+        //     utt: Math.round(time)
+        // });
     }
 
     trackPageView() {
         return this._collect({
-            t: 'pageview'
+            hit: {
+                type: 'pageview'
+            }
         });
     }
 
     trackEvent(action, label, value, category = UNKNOWN) {
         return this._collect({
-            t: 'event',
-            ec: category,
-            ea: action,
-            el: label,
-            ev: value
+            hit: {
+                type: 'event'
+            },
+            event: {
+                category,
+                action,
+                label,
+                value
+            }
         });
+        // return this._collect({
+        //     t: 'event',
+        //     ec: category,
+        //     ea: action,
+        //     el: label,
+        //     ev: value
+        // });
     }
 
     printAboutAnalytics() {
