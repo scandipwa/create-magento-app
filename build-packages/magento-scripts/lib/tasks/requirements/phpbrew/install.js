0076071d5c1ddf258af3fc3da4d733775494b331
Okabe Rintaro <ejnshtein@gmail.com>
2022-05-30 16:59:56 +0300

    stage changes for installing dependencies on arm macs in rosetta

--- build-packages/magento-scripts/lib/tasks/requirements/phpbrew/install.js
+++ build-packages/magento-scripts/lib/tasks/requirements/phpbrew/install.js
@@ -6,6 +6,7 @@ const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const installDependenciesTask = require('../../../util/install-dependencies-task');
 const osPlatform = require('../../../util/os-platform');
 const KnownError = require('../../../errors/known-error');
+const { getBrewCommand } = require('../../../util/get-brew-bin-path');
 
 /**
  * @type {() => import('listr2').ListrTask<import('../../../../typings/context').ListrContext>}
@@ -14,10 +15,20 @@ const installPHPBrewDependencies = () => ({
     title: 'Installing PHPBrew dependencies',
     task: async (ctx, task) => {
         if (process.platform === 'darwin') {
+            const installedDependencies = (await execAsyncSpawn(`${await getBrewCommand({ native: false })} list`)).split('\n');
+
+            const dependenciesToInstall = ['php', 'autoconf', 'pkg-config'].filter((dep) => !installedDependencies.includes(dep));
+
+            if (dependenciesToInstall.length === 0) {
+                task.skip();
+                return;
+            }
+
             return task.newListr(
                 installDependenciesTask({
                     platform: 'darwin',
-                    dependenciesToInstall: ['php', 'autoconf', 'pkg-config']
+                    dependenciesToInstall,
+                    useMacNativeEnvironment: false
                 })
             );
         }
