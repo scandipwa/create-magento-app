39fa29044b12f33024d5369aee9c40ea8cf1da99
Okabe Rintaro <ejnshtein@gmail.com>
2022-06-03 16:13:33 +0300

    stage minor change

--- build-packages/magento-scripts/lib/tasks/composer/local-auth-json.js
+++ build-packages/magento-scripts/lib/tasks/composer/local-auth-json.js
@@ -5,14 +5,16 @@ const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const UnknownError = require('../../errors/unknown-error');
 const KnownError = require('../../errors/known-error');
 
+const authJsonPath = path.join(process.cwd(), 'auth.json');
+
 /**
  * @type {() => import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
  */
 const localAuthJson = () => ({
     task: async (ctx, task) => {
-        if (await pathExists(path.join(process.cwd(), 'auth.json'))) {
+        if (await pathExists(authJsonPath)) {
             task.title = 'Using local auth.json';
-            const localAuthJson = await fs.promises.readFile(path.join(process.cwd(), 'auth.json'));
+            const localAuthJson = await fs.promises.readFile(authJsonPath);
 
             let localAuthJsonContent;
             try {
