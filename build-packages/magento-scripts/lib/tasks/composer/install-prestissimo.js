4cecab9d8a6687f96c834e50f961e13d55b944bc
Okabe Rintaro <ejnshtein@gmail.com>
2022-06-28 19:35:59 +0300

    add prompt to disable prestissimo in composer.json allowed-plugins

--- build-packages/magento-scripts/lib/tasks/composer/install-prestissimo.js
+++ build-packages/magento-scripts/lib/tasks/composer/install-prestissimo.js
@@ -1,5 +1,16 @@
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
 const semver = require('semver');
+const { baseConfig } = require('../../config');
+const getJsonfileData = require('../../util/get-jsonfile-data');
+const pathExists = require('../../util/path-exists');
 const runComposerCommand = require('../../util/run-composer');
+
+const globalComposerJsonPath = path.join(os.homedir(), '.config', 'composer', 'composer.json');
+const localComposerJsonPath = path.join(baseConfig.magentoDir, 'composer.json');
+const prestissimoPluginName = 'hirak/prestissimo';
 
 /**
  * @type {() => import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
@@ -10,10 +21,49 @@ const prestissimoInstall = () => ({
         const { magentoVersion, composerVersion } = ctx;
 
         if (semver.satisfies(composerVersion, '^2')) {
+            if (await pathExists(globalComposerJsonPath) && semver.satisfies(composerVersion, '^2.2.0')) {
+                const globalComposerJsonData = await getJsonfileData(globalComposerJsonPath);
+                const hasPrestissimoInstalled = globalComposerJsonData
+                    && globalComposerJsonData.require
+                    && !!globalComposerJsonData.require[prestissimoPluginName];
+
+                if (await pathExists(localComposerJsonPath)) {
+                    const localComposerJsonData = await getJsonfileData(localComposerJsonPath);
+
+                    if (
+                        hasPrestissimoInstalled
+                            && localComposerJsonData
+                            && localComposerJsonData.config
+                            && localComposerJsonData.config['allow-plugins']
+                            && typeof localComposerJsonData.config['allow-plugins'][prestissimoPluginName] !== 'boolean'
+                    ) {
+                        const disableConfirmation = await task.prompt({
+                            type: 'Select',
+                            message: `We noticed that you have Composer ${ctx.composerVersion}, but you have ${logger.style.code(prestissimoPluginName)} installed, which is used only in Composer 1.
+Would you like to disable it in your project?`,
+                            choices: [
+                                {
+                                    name: 'disable',
+                                    message: 'Disable it, thanks'
+                                },
+                                {
+                                    name: 'skip',
+                                    message: 'Skip this step'
+                                }
+                            ]
+                        });
+
+                        if (disableConfirmation === 'disable') {
+                            localComposerJsonData.config['allow-plugins'][prestissimoPluginName] = false;
+                            await fs.promises.writeFile(localComposerJsonPath, JSON.stringify(localComposerJsonData, null, 4), 'utf-8');
+                        }
+                    }
+                }
+            }
             task.skip();
             return;
         }
-        const { code } = await runComposerCommand('global show hirak/prestissimo', {
+        const { code } = await runComposerCommand(`global show ${prestissimoPluginName}`, {
             throwNonZeroCode: false,
             magentoVersion
         });
@@ -23,7 +73,7 @@ const prestissimoInstall = () => ({
             return;
         }
 
-        await runComposerCommand('global require hirak/prestissimo', {
+        await runComposerCommand(`global require ${prestissimoPluginName}`, {
             magentoVersion,
             callback: (t) => {
                 task.output = t;
