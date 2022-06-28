10b808c212851e447998fc225575d5df7f2def3b
Okabe Rintaro <ejnshtein@gmail.com>
2022-06-28 19:31:10 +0300

    fixed check for running process as root

--- build-packages/magento-scripts/lib/util/is-running-root.js
+++ build-packages/magento-scripts/lib/util/is-running-root.js
@@ -1,3 +1,5 @@
-const isRunningRoot = () => process.getuid() === 0; // UID 0 is always root
+const os = require('os');
+
+const isRunningRoot = () => ['linux', 'darwin'].includes(os.platform()) && process.getuid() === 0; // UID 0 is always root
 
 module.exports = isRunningRoot;
