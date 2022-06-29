ce4aa8991ccc11134f75631ef4a363cf88801116
Okabe Rintaro <ejnshtein@gmail.com>
2022-06-29 18:01:29 +0300

    add secure-file-priv=NULL command option to mysql container

--- build-packages/magento-scripts/lib/config/docker.js
+++ build-packages/magento-scripts/lib/config/docker.js
@@ -206,7 +206,8 @@ module.exports = async ({ configuration, ssl, host }, config) => {
                     '--log_bin_trust_function_creators=1',
                     '--default-authentication-plugin=mysql_native_password',
                     '--max_allowed_packet=1GB',
-                    '--bind-address=0.0.0.0'
+                    '--bind-address=0.0.0.0',
+                    '--secure-file-priv=NULL'
                 ].join(' '),
                 securityOptions: [
                     'seccomp=unconfined'
