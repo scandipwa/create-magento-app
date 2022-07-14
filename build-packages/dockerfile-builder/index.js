d60436a70572e3c58c3bf1dda85a57d5ba304ad0
Okabe Rintaro <ejnshtein@gmail.com>
2022-07-14 15:57:22 +0300

    add dockerfile-builder package

---
+++ build-packages/dockerfile-builder/index.js
@@ -0,0 +1,272 @@
+const { buildInstructions } = require('./build-instructions');
+
+class DockerFileBuilder {
+    /**
+     * @type {import('./types').DockerfileInstruction[]}
+     */
+    instructions = [];
+
+    /**
+     * @param {Omit<import('./types').FromInstruction, 'type'>} param0
+     * @returns {this}
+     */
+    from({
+        image,
+        tag = 'latest',
+        platform
+    }) {
+        this.instructions.push({
+            type: 'FROM',
+            image,
+            tag,
+            platform
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').RunInstruction['command']} command
+     */
+    run(command) {
+        this.instructions.push({
+            type: 'RUN',
+            command
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').CmdInstruction['command']} command
+     */
+    cmd(command) {
+        this.instructions.push({
+            type: 'CMD',
+            command
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').LabelInstruction['labels']} labels
+     */
+    label(labels) {
+        this.instructions.push({
+            type: 'LABEL',
+            labels
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Omit<import('./types').ExposeInstruction, 'type'>} param0
+     */
+    expose({
+        port,
+        protocol
+    }) {
+        this.instructions.push({
+            type: 'EXPOSE',
+            port,
+            protocol
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Record<string, string>} env
+     */
+    env(env) {
+        Object.entries(env).forEach(([name, value]) => {
+            this.instructions.push({
+                type: 'ENV',
+                name,
+                value
+            });
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Omit<import('./types').AddInstruction, 'type'>} param0
+     */
+    add({
+        src,
+        dest,
+        chown
+    }) {
+        this.instructions.push({
+            type: 'ADD',
+            src,
+            dest,
+            chown
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Omit<import('./types').CopyInstruction, 'type'>} param0
+     */
+    copy({
+        src,
+        dest,
+        chown
+    }) {
+        this.instructions.push({
+            type: 'COPY',
+            src,
+            dest,
+            chown
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').EntrypointInstruction['command']} command
+     */
+    entrypoint(command) {
+        this.instructions.push({
+            type: 'ENTRYPOINT',
+            command
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').VolumeInstruction['volume']} volume
+     */
+    volume(volume) {
+        this.instructions.push({
+            type: 'VOLUME',
+            volume
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').UserInstruction['user']} user
+     */
+    user(user) {
+        this.instructions.push({
+            type: 'USER',
+            user
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').WorkDirInstruction['workdir']} workdir
+     */
+    workDir(workdir) {
+        this.instructions.push({
+            type: 'WORKDIR',
+            workdir
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Omit<import('./types').ArgInstruction, 'type'>} param0
+     */
+    arg({
+        name,
+        defaultValue
+    }) {
+        this.instructions.push({
+            type: 'ARG',
+            name,
+            defaultValue
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').OnBuildInstruction['instruction']} instruction
+     */
+    onBuild(instruction) {
+        this.instructions.push({
+            type: 'ONBUILD',
+            instruction
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').StopSignalInstruction['signal']} signal
+     */
+    stopSignal(signal) {
+        this.instructions.push({
+            type: 'STOPSIGNAL',
+            signal
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {Omit<import('./types').HealthCheckInstruction, 'type'>} param0
+     */
+    healthCheck({
+        command,
+        interval,
+        timeout,
+        startPeriod,
+        retries
+    }) {
+        this.instructions.push({
+            type: 'HEALTHCHECK',
+            command,
+            interval,
+            retries,
+            startPeriod,
+            timeout
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').ShellInstruction['exec']} exec
+     */
+    shell(exec) {
+        this.instructions.push({
+            type: 'SHELL',
+            exec
+        });
+
+        return this;
+    }
+
+    /**
+     * @param {import('./types').CommentInstruction['comment']} comment
+     */
+    comment(comment) {
+        this.instructions.push({
+            type: 'COMMENT',
+            comment
+        });
+
+        return this;
+    }
+
+    build() {
+        return buildInstructions(this.instructions);
+    }
+}
+
+module.exports = {
+    DockerFileBuilder
+};
---
+++ build-packages/dockerfile-builder/types.d.ts
@@ -0,0 +1,121 @@
+export interface FromInstruction {
+    type: 'FROM'
+    image: string
+    tag?: string
+    platform?: string
+}
+
+export interface RunInstruction {
+    type: 'RUN'
+    command: string | string[]
+}
+
+export interface CmdInstruction {
+    type: 'CMD'
+    command: string | string[]
+}
+
+export interface LabelInstruction {
+    type: 'LABEL'
+    labels: Record<string, string>
+}
+
+export interface ExposeInstruction {
+    type: 'EXPOSE'
+    port: number
+    protocol?: 'udp' | 'tcp'
+}
+
+export interface EnvInstruction {
+    type: 'ENV'
+    name: string
+    value: string
+}
+
+export interface AddInstruction {
+    type: 'ADD'
+    src: string[]
+    dest: string
+    chown?: string
+}
+
+export interface CopyInstruction {
+    type: 'COPY'
+    src: string[]
+    dest: string
+    chown?: string
+}
+
+export interface EntrypointInstruction {
+    type: 'ENTRYPOINT'
+    command: string | string[]
+}
+
+export interface VolumeInstruction {
+    type: 'VOLUME'
+    volume: string | string[]
+}
+
+export interface UserInstruction {
+    type: 'USER'
+    user: string
+}
+
+export interface WorkDirInstruction {
+    type: 'WORKDIR'
+    workdir: string
+}
+
+export interface ArgInstruction {
+    type: 'ARG'
+    name: string
+    defaultValue?: string
+}
+
+export interface OnBuildInstruction {
+    type: 'ONBUILD'
+    instruction: string
+}
+
+export interface StopSignalInstruction {
+    type: 'STOPSIGNAL'
+    signal: string
+}
+
+export interface HealthCheckInstruction {
+    type: 'HEALTHCHECK'
+    command: string
+    interval?: string
+    timeout?: string
+    startPeriod?: string
+    retries?: number
+}
+
+export interface ShellInstruction {
+    type: 'SHELL'
+    exec: string[]
+}
+
+export interface CommentInstruction {
+    type: 'COMMENT'
+    comment: string
+}
+
+export type DockerfileInstruction = FromInstruction
+    | RunInstruction
+    | CmdInstruction
+    | LabelInstruction
+    | ExposeInstruction
+    | EnvInstruction
+    | AddInstruction
+    | CopyInstruction
+    | EntrypointInstruction
+    | VolumeInstruction
+    | UserInstruction
+    | WorkDirInstruction
+    | ArgInstruction
+    | OnBuildInstruction
+    | StopSignalInstruction
+    | HealthCheckInstruction
+    | ShellInstruction
+    | CommentInstruction
