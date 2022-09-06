c6d25be4ecf1c83749ea432b575e506c3181cc2a
Okabe Rintaro <ejnshtein@gmail.com>
2022-09-06 17:13:18 +0300

    refactor systemctl utility to include exists method

--- build-packages/magento-scripts/lib/util/systemctl.js
+++ build-packages/magento-scripts/lib/util/systemctl.js
@@ -3,10 +3,10 @@ const { execAsyncSpawn } = require('./exec-async-command');
 /**
  * @param {String} cmd
  * @param {String} serviceName
- * @param {{ now: boolean }} options
+ * @param {{ now: boolean, user: boolean }} options
  */
 const run = (cmd, serviceName, options = {}) => execAsyncSpawn(
-    `systemctl ${ cmd }${ serviceName ? ` ${ serviceName }` : '' }${ options.now ? ' --now' : ''}`,
+    `systemctl ${ options.user ? '--user ' : '' }${ cmd }${ serviceName ? ` ${ serviceName }` : '' }${ options.now ? ' --now' : ''}`,
     {
         withCode: true
     }
@@ -14,26 +14,75 @@ const run = (cmd, serviceName, options = {}) => execAsyncSpawn(
 
 const daemonReload = () => run('daemon-reload');
 
-const systemctlControl = (serviceName) => ({
-    disable: () => run('disable', serviceName),
-    enable: () => run('enable', serviceName),
-    enableAndStart: () => run('enable', serviceName, { now: true }),
-    restart: () => run('restart', serviceName),
-    start: () => run('start', serviceName),
-    stop: () => run('stop', serviceName),
-    isEnabled: async () => {
+/**
+ * @param {String} serviceName
+ * @param {{ now: boolean, user: boolean }} defaultOptions
+ */
+const systemctlControl = (serviceName, defaultOptions = {}) => ({
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    disable: (options = {}) => run('disable', serviceName, { ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    enable: (options = {}) => run('enable', serviceName, { ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    enableAndStart: (options = {}) => run('enable', serviceName, { now: true, ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    restart: (options = {}) => run('restart', serviceName, { ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    start: (options = {}) => run('start', serviceName, { ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    stop: (options = {}) => run('stop', serviceName, { ...defaultOptions, ...options }),
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    isEnabled: async (options = {}) => {
         try {
-            const { result } = await run('is-enabled', serviceName);
+            const { result } = await run('is-enabled', serviceName, { ...defaultOptions, ...options });
             return result.includes('enabled');
         } catch (e) {
             return false;
         }
     },
-    isRunning: async () => {
+    /**
+     * @param {{ now: boolean, user: boolean }} options
+     */
+    isRunning: async (options = {}) => {
         try {
-            const { result } = await run('status', serviceName);
+            const { result } = await run('status', serviceName, { ...defaultOptions, ...options });
 
             return result.includes('active (running)');
+        } catch (e) {
+            return false;
+        }
+    },
+    /**
+     * @param {{ user: boolean }} options
+     */
+    exists: async (options = {}) => {
+        const optionsToUse = { ...defaultOptions, ...options };
+        try {
+            // eslint-disable-next-line max-len, no-useless-escape
+            const command = `if [[ $(systemctl ${ optionsToUse.user ? '--user ' : ''}list-units --all -t service --full --no-legend "${ serviceName }.service" | sed 's/^\\s*//g' | cut -f1 -d' ') == ${ serviceName }.service ]]; then
+                echo "1"
+            else
+                echo "0"
+            fi`;
+            const result = await execAsyncSpawn(
+                command
+            );
+
+            return result === '1';
         } catch (e) {
             return false;
         }
