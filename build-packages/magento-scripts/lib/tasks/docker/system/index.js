fe37bde7e6564574f6af085d43fa804f08b2c2d7
Okabe Rintaro <ejnshtein@gmail.com>
2022-09-15 14:15:01 +0300

    add docker system api wrapper

---
+++ build-packages/magento-scripts/lib/tasks/docker/system/index.js
@@ -0,0 +1,5 @@
+const systemApi = require('./system-api');
+
+module.exports = {
+    systemApi
+};
---
+++ build-packages/magento-scripts/lib/tasks/docker/system/system-api.d.ts
@@ -0,0 +1,71 @@
+import { ExecAsyncSpawnOptions } from '../../../util/exec-async-command';
+
+export interface SystemDFOptions<T extends boolean = false> {
+    format?: string
+    formatToJSON?: T
+    verbose?: boolean
+}
+
+export interface SystemDFResult {
+    Images: {
+        Containers: string
+        CreatedAt: string
+        CreatedSince: string
+        Digest: string
+        ID: string
+        Repository: string
+        SharedSize: string
+        Size: string
+        Tag: string
+        UniqueSize: string
+        VirtualSize: string
+    }[]
+    Containers: {
+        Command: string
+        CreatedAt: string
+        ID: string
+        Image: string
+        Labels: string
+        LocalVolumes: string
+        Mounts: string
+        Names: string
+        Networks: string
+        Ports: StreamPipeOptions
+        RunningFor: string
+        Size: string
+        State: string
+        Status: string
+    }[]
+    Volumes: {
+        Driver: string
+        Labels: string
+        Links: string
+        Mountpoint: string
+        Name: string
+        Scope: string
+        Size: string
+    }[]
+    BuildCache: {
+        CacheType: string
+        CreatedAt: string
+        CreatedSince: string
+        Description: string
+        ID: string
+        InUse: string
+        LastUsedAt: string
+        LastUsedSince: string
+        Parent: string
+        Shared: string
+        Size: string
+        UsageCount: string
+    }[]
+}
+
+export function df(
+    options?: SystemDFOptions,
+    execOptions?: ExecAsyncSpawnOptions<false>
+): Promise<string>
+export function df(
+    options?: SystemDFOptions<true>,
+    execOptions?: ExecAsyncSpawnOptions<false>
+): Promise<SystemDFResult>
