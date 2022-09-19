634cd89f05e613d7315e64500826bc1f96b4bd86
Okabe Rintaro <ejnshtein@gmail.com>
2022-09-19 18:10:33 +0300

    add validation for theme location during link command

--- build-packages/magento-scripts/lib/tasks/theme/retrieve-theme-data.js
+++ build-packages/magento-scripts/lib/tasks/theme/retrieve-theme-data.js
@@ -1,3 +1,4 @@
+const logger = require('@scandipwa/scandipwa-dev-utils/logger');
 const path = require('path');
 const KnownError = require('../../errors/known-error');
 const getJsonfileData = require('../../util/get-jsonfile-data');
@@ -14,7 +15,7 @@ const retrieveThemeData = (themePath) => ({
         // check if path not relative
         if (!(await pathExists(path.join(absoluteThemePath, 'composer.json')))) {
             // if composer.json is not found, then it's not correct path
-            // and we need to test if it's alsolute one
+            // and we need to test if it's absolute one
             if (await pathExists(path.join(themePath, 'composer.json'))) {
                 // if so, use it as absolute path
                 absoluteThemePath = themePath;
@@ -22,6 +23,15 @@ const retrieveThemeData = (themePath) => ({
         } else {
             // path is relative, so we use it
             absoluteThemePath = themePath;
+        }
+
+        // validate if theme is located inside magento directory
+        if (!absoluteThemePath.includes(process.cwd())) {
+            throw new KnownError(`You are trying to link the theme from outside of your Magento project!
+
+This is not supported in ${ logger.style.command('magento-scripts') } version 2.
+
+Move your theme inside Magento project!`);
         }
 
         const composerData = await getJsonfileData(path.join(absoluteThemePath, 'composer.json'));
