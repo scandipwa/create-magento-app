a84b0a63e5225acda3d52eb4a61004b132dc9c36
Okabe Rintaro <ejnshtein@gmail.com>
2022-10-12 18:46:55 +0300

    use correct configs for magento 2.4.4-p2, 2.4.5-p1

--- build-packages/magento-scripts/lib/config/versions/magento-2.4.5-p1.js
+++ build-packages/magento-scripts/lib/config/versions/magento-2.4.5-p1.js
@@ -1,26 +1,21 @@
+const path = require('path');
 const { defaultMagentoConfig } = require('../magento-config');
-const sodium = require('../services/php/extensions/sodium');
-const { magento24PHPExtensionList } = require('../magento/required-php-extensions');
-const { php81 } = require('../services/php/versions');
-const { sslTerminator } = require('../services/ssl-terminator');
-const { varnish70 } = require('../services/varnish');
-const { repo } = require('../services/php/base-repo');
-const { nginx118 } = require('../services/nginx/versions');
-const { composer2 } = require('../services/composer/versions');
-const { maildev } = require('../services/maildev');
-const { redis62 } = require('../services/redis');
+const { php81 } = require('../php/releases');
+const { sslTerminator } = require('../ssl-terminator');
+const { varnish70 } = require('../varnish/varnish-7-0');
 
 module.exports = ({ templateDir } = {}) => ({
     magentoVersion: '2.4.5-p1',
     isDefault: true,
     configuration: {
-        php: php81({
-            templateDir,
-            extensions: { ...magento24PHPExtensionList, sodium },
-            baseImage: `${ repo }:php-8.1-magento-2.4`
-        }),
-        nginx: nginx118({ templateDir }),
-        redis: redis62(),
+        php: php81({ templateDir }),
+        nginx: {
+            version: '1.18.0',
+            configTemplate: path.join(templateDir || '', 'nginx.template.conf')
+        },
+        redis: {
+            version: '6.0'
+        },
         mysql: {
             version: '8.0'
         },
@@ -30,10 +25,11 @@ module.exports = ({ templateDir } = {}) => ({
         elasticsearch: {
             version: '7.17.6'
         },
-        composer: composer2(),
+        composer: {
+            version: '2'
+        },
         varnish: varnish70({ templateDir }),
-        sslTerminator: sslTerminator({ templateDir }),
-        maildev: maildev()
+        sslTerminator: sslTerminator({ templateDir })
     },
     magento: defaultMagentoConfig,
     host: 'localhost',
