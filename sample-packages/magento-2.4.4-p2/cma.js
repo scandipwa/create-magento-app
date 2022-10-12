4bc75920929a6f4c6fbac24bfba904571260da3b
Okabe Rintaro <ejnshtein@gmail.com>
2022-10-12 19:06:53 +0300

    add sample magento 2.4.4-p2, 2.4.5-p1

---
+++ sample-packages/magento-2.4.4-p2/cma.js
@@ -0,0 +1,14 @@
+/** @type {import('@scandipwa/magento-scripts').CMAConfiguration} */
+module.exports = {
+    magento: {
+        first_name: 'Scandiweb',
+        last_name: 'Developer',
+        email: 'developer@scandipwa.com',
+        user: 'admin',
+        password: 'scandipwa123',
+        adminuri: 'admin',
+        mode: 'developer',
+        edition: 'community'
+    },
+    configuration: {}
+};
