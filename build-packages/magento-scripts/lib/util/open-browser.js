b27b742e4ea9aa4b2a3aa956afb5ccde8952c0c1
Okabe Rintaro <ejnshtein@gmail.com>
2022-11-17 15:04:28 +0200

    fix opening styled url in the browser after start command is done

--- build-packages/magento-scripts/lib/util/open-browser.js
+++ build-packages/magento-scripts/lib/util/open-browser.js
@@ -1,12 +1,14 @@
 const { execAsync } = require('./exec-async')
 
+// eslint-disable-next-line no-control-regex
+const consoleStyleReplacer = /[\u001b]\[\S+?m/g
 /**
  * @param {string} url
  */
 const openBrowser = async (url) => {
     const start = process.platform === 'darwin' ? 'open' : 'xdg-open'
 
-    await execAsync(`${start} ${url}`)
+    await execAsync(`${start} ${url.replace(consoleStyleReplacer, '')}`)
 }
 
 module.exports = openBrowser
