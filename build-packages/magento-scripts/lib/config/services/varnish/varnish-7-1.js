ab2c72d71ed777a95c61f5cbd028171b42c8f845
Samuel Colacchia <samuel@metalcloak.com>
2023-03-20 12:28:08 -0700

    Added Varnish 7.1

---
+++ build-packages/magento-scripts/lib/config/services/varnish/varnish-7-1.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+/**
+ * @param {Object} param0
+ * @param {string} param0.templateDir
+ * @returns {import('../../../../typings/index').VarnishConfiguration}
+ */
+const varnish71 = ({ templateDir }) => ({
+    enabled: true,
+    healthCheck: false,
+    image: 'varnish:7.1',
+    configTemplate: path.join(templateDir || '', 'varnish.template.vcl')
+})
+
+module.exports = varnish71
