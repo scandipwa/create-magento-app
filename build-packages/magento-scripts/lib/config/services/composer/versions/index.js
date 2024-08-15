1b504598b3ef4cf7ee4084397607f7d8288fc11f
Aleksandrs Rivkinds <ejnshtein@gmail.com>
2024-08-15 12:17:14 +0300

    add support for Magento 2.4.4-p10, 2.4.5-p9, 2.4.6-p7, 2.4.7-p2. set 2.4.7-p2 as default version

--- build-packages/magento-scripts/lib/config/services/composer/versions/index.js
+++ build-packages/magento-scripts/lib/config/services/composer/versions/index.js
@@ -2,5 +2,6 @@ module.exports = {
     composer1: require('./composer-1'),
     composer2: require('./composer-2'),
     composer22: require('./composer-2.2'),
-    composer26: require('./composer-2.6')
+    composer26: require('./composer-2.6'),
+    composer27: require('./composer-2.7')
 }
