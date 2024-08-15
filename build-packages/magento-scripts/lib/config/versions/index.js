9b7a74336a2312603462aba0d57f78f89b129b8d
Aleksandrs Rivkinds <ejnshtein@gmail.com>
2024-08-15 12:21:28 +0300

    update magento version sorting algorithm

--- build-packages/magento-scripts/lib/config/versions/index.js
+++ build-packages/magento-scripts/lib/config/versions/index.js
@@ -33,6 +33,9 @@ const magentoVersions = fs
     .filter((f) => f.name.includes('magento'))
     .map((f) => require(path.join(__dirname, f.name)))
 
+/**
+ * @returns {Record<string, ReturnType<import('../../../typings/common').MagentoVersionConfigurationFunction>>}
+ */
 const getConfigurations = (config = {}) =>
     magentoVersions.reduce(
         (acc, val) => ({
@@ -46,7 +49,46 @@ const getConfigurations = (config = {}) =>
 
 const allVersions = Object.entries(getConfigurations())
     .map(([name, magentoConfig]) => ({ ...magentoConfig, name }))
-    .sort((a, b) => a.magentoVersion.localeCompare(b.magentoVersion))
+    .sort((a, b) => {
+        // Splitting the version strings into major, minor, and patch numbers
+        const [versionA, patchA] = a.magentoVersion.split('-')
+        const [versionB, patchB] = b.magentoVersion.split('-')
+
+        const [majorA, minorA, patchNumberA] = versionA.split('.').map(Number)
+        const [majorB, minorB, patchNumberB] = versionB.split('.').map(Number)
+
+        // Comparing major versions
+        if (majorA !== majorB) {
+            return majorA - majorB
+        }
+        // Comparing minor versions
+        if (minorA !== minorB) {
+            return minorA - minorB
+        }
+        // Comparing patch numbers
+        if (patchNumberA !== patchNumberB) {
+            return patchNumberA - patchNumberB
+        }
+
+        // Comparing patch versions (if present)
+        if (patchA && patchB) {
+            const patchNumberA = Number(patchA.replace('p', ''))
+            const patchNumberB = Number(patchB.replace('p', ''))
+            return patchNumberA - patchNumberB
+        }
+
+        // Sorting versions with patch against versions without patch
+        if (patchA && !patchB) {
+            return -1
+        }
+
+        if (!patchA && patchB) {
+            return 1
+        }
+
+        // Versions are equal
+        return 0
+    })
     .reverse()
 
 module.exports = {
