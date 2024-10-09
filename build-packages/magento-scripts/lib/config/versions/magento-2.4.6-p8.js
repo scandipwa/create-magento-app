d84b5839d988d9592669e3896f2ad9b187d7170e
Aleksandrs Rivkinds <ejnshtein@gmail.com>
2024-10-09 18:04:27 +0300

    add support for magento 2.4.8-beta1, 2.4.7-p3, 2.4.6-p8, 2.4.5-p10, 2.4.4-p11 set default magento version to 2.4.7-p3

--- build-packages/magento-scripts/lib/config/versions/magento-2.4.6-p8.js
+++ build-packages/magento-scripts/lib/config/versions/magento-2.4.6-p8.js
@@ -2,16 +2,16 @@ const sodium = require('../services/php/extensions/sodium')
 const {
     magento24PHPExtensionList
 } = require('../magento/required-php-extensions')
-const { php82 } = require('../services/php/versions')
+const { php81 } = require('../services/php/versions')
 const { sslTerminator } = require('../services/ssl-terminator')
-const { varnish74 } = require('../services/varnish')
+const { varnish73 } = require('../services/varnish')
 const { repo } = require('../services/php/base-repo')
 const { nginx124 } = require('../services/nginx/versions')
-const { composer27 } = require('../services/composer/versions')
+const { composer22 } = require('../services/composer/versions')
 const { maildev } = require('../services/maildev')
-const { redis72 } = require('../services/redis')
+const { redis70 } = require('../services/redis')
 const { mariadb106 } = require('../services/mariadb/versions')
-const { elasticsearch811 } = require('../services/elasticsearch/versions')
+const { elasticsearch85 } = require('../services/elasticsearch/versions')
 const { mysql80 } = require('../services/mysql/versions')
 const { opensearch212 } = require('../services/opensearch/versions')
 
@@ -19,20 +19,20 @@ const { opensearch212 } = require('../services/opensearch/versions')
  * @type {import('../../../typings/common').MagentoVersionConfigurationFunction}
  */
 module.exports = ({ templateDir }) => ({
-    magentoVersion: '2.4.7-p2',
+    magentoVersion: '2.4.6-p8',
     configuration: {
-        php: php82({
+        php: php81({
             templateDir,
             extensions: { ...magento24PHPExtensionList, sodium },
-            baseImage: `${repo}:php-8.2-magento-2.4`
+            baseImage: `${repo}:php-8.1-magento-2.4`
         }),
         nginx: nginx124({ templateDir }),
-        redis: redis72(),
+        redis: redis70(),
         mysql: mysql80(),
         mariadb: mariadb106(),
-        elasticsearch: elasticsearch811(),
-        composer: composer27(),
-        varnish: varnish74({ templateDir }),
+        elasticsearch: elasticsearch85(),
+        composer: composer22(),
+        varnish: varnish73({ templateDir }),
         sslTerminator: sslTerminator({ templateDir }),
         maildev: maildev(),
         opensearch: opensearch212()
