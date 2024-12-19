e583338a0cb96c477429a27dc1028cf064dd9fe8
Denis Protassoff <denis.protassoff@scandiweb.com>
2024-12-19 21:56:49 +0300

    Add SourceGuardian extension

--- build-packages/magento-scripts-php-extensions/index.js
+++ build-packages/magento-scripts-php-extensions/index.js
@@ -4,11 +4,13 @@ const sqlsrv = require('./extensions/sqlsrv')
 const ioncube = require('./extensions/ioncube')
 const memcached = require('./extensions/memcached')
 const imagick = require('./extensions/imagick')
+const sourceguardian = require('./extensions/sourceguardian')
 
 module.exports = {
     sqlsrv,
     pdo_sqlsrv,
     ioncube,
     memcached,
-    imagick
+    imagick,
+    sourceguardian
 }
