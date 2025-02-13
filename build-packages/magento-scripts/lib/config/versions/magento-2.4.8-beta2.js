aa7d33d1292ca92f9658b0658049ff6b2f9869d2
Aleksandrs Rivkinds <ejnshtein@gmail.com>
2025-02-13 15:10:56 +0200

    TEAMS-1029 - Add Magento 2.4.8-beta2 support - New system requirements -MariaDB 10.6, Redis 7.2, Varnish 7.6, Nginx 1.26, Composer 2.8

---
+++ build-packages/magento-scripts/lib/config/versions/magento-2.4.8-beta2.js
@@ -0,0 +1,41 @@
+const sodium = require('../services/php/extensions/sodium')
+const {
+    magento24PHPExtensionList
+} = require('../magento/required-php-extensions')
+const { php82 } = require('../services/php/versions')
+const { sslTerminator } = require('../services/ssl-terminator')
+const { varnish76 } = require('../services/varnish')
+const { repo } = require('../services/php/base-repo')
+const { nginx126 } = require('../services/nginx/versions')
+const { composer28 } = require('../services/composer/versions')
+const { maildev } = require('../services/maildev')
+const { redis72 } = require('../services/redis')
+const { mariadb106 } = require('../services/mariadb/versions')
+const { elasticsearch816 } = require('../services/elasticsearch/versions')
+const { mysql80 } = require('../services/mysql/versions')
+const { opensearch212 } = require('../services/opensearch/versions')
+
+/**
+ * @type {import('../../../typings/common').MagentoVersionConfigurationFunction}
+ */
+module.exports = ({ templateDir }) => ({
+    magentoVersion: '2.4.8-beta2',
+    configuration: {
+        php: php82({
+            templateDir,
+            extensions: { ...magento24PHPExtensionList, sodium },
+            baseImage: `${repo}:php-8.2-magento-2.4`
+        }),
+        nginx: nginx126({ templateDir }),
+        redis: redis72(),
+        mysql: mysql80(),
+        mariadb: mariadb106(),
+        elasticsearch: elasticsearch816(),
+        composer: composer28(),
+        varnish: varnish76({ templateDir }),
+        sslTerminator: sslTerminator({ templateDir }),
+        maildev: maildev(),
+        opensearch: opensearch212(),
+        searchengine: 'opensearch'
+    }
+})
