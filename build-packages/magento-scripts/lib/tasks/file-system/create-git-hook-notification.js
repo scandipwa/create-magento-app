50c47260bfcde719f942ed4a628661a8aa4fc0c9
Okabe Rintaro <ejnshtein@gmail.com>
2025-08-13 23:10:47 +0300

    Improve stability of git hook notification creation

--- build-packages/magento-scripts/lib/tasks/file-system/create-git-hook-notification.js
+++ build-packages/magento-scripts/lib/tasks/file-system/create-git-hook-notification.js
@@ -1,58 +1,112 @@
 const path = require('path')
 const fs = require('fs')
 const UnknownError = require('../../errors/unknown-error')
-const setConfigFile = require('../../util/set-config')
 const { execAsyncSpawn } = require('../../util/exec-async-command')
+const pathExists = require('../../util/path-exists')
+
+/**
+ * @param {string} p
+ */
+function isExec(p) {
+    try {
+        fs.accessSync(p, fs.constants.X_OK)
+        return true
+    } catch (e) {
+        return false
+    }
+}
 
 /**
  * @returns {import('listr2').ListrTask<import('../../../typings/context').ListrContext>}
  */
 const createGitHookNotification = () => ({
-    title: 'Setting git hook notification',
-    task: async (ctx) => {
+    title: 'Setting Git Hook Notification',
+    task: async (ctx, task) => {
         const gitHookTemplatePath = path.join(
             ctx.config.baseConfig.templateDir,
             'git-postcheckout-hook.template'
         )
 
-        const gitRoot = await execAsyncSpawn('git rev-parse --show-toplevel')
-
-        let currentGitHookPath = await execAsyncSpawn(
-            `git config --get core.hooksPath`
-        )
-
-        if (!currentGitHookPath.trim()) {
-            await execAsyncSpawn(`git config core.hooksPath .git/hooks`)
-
-            currentGitHookPath = '.git/hooks'
-        }
-
-        const gitHookPath = path.join(
-            gitRoot,
-            currentGitHookPath,
-            'post-checkout'
-        )
-
-        try {
-            await setConfigFile({
-                configPathname: gitHookPath,
-                template: gitHookTemplatePath,
-                overwrite: false
+        const [gitRootResult, currentGitHookPathResult] = await Promise.all([
+            execAsyncSpawn('git rev-parse --show-toplevel', {
+                withCode: true
+            }),
+            execAsyncSpawn(`git config --get core.hooksPath`, {
+                withCode: true
             })
-        } catch (e) {
+        ])
+
+        if (currentGitHookPathResult.code !== 0) {
             throw new UnknownError(
-                `Unexpected error accrued during git hook notification creation\n\n${e}`
+                `Unexpected error accrued during git hook notification creation\n\n${currentGitHookPathResult.result}`
             )
         }
 
-        // check if gitHookPath is executable
-        const isExecutable = await fs.promises.stat(gitHookPath)
-        if (isExecutable.mode & 0o111) {
-            return
+        if (gitRootResult.code !== 0) {
+            throw new UnknownError(
+                `Unexpected error accrued during git hook notification creation\n\n${gitRootResult.result}`
+            )
         }
 
-        await execAsyncSpawn(`chmod +x ${gitHookPath}`)
-    }
+        const tasks = []
+
+        if (!currentGitHookPathResult.result.trim()) {
+            tasks.push({
+                title: 'Setting Git Hook Path',
+                task: async () => {
+                    const { code } = await execAsyncSpawn(
+                        `git config core.hooksPath .git/hooks`,
+                        {
+                            withCode: true
+                        }
+                    )
+
+                    if (code !== 0) {
+                        throw new UnknownError(
+                            `Unexpected error accrued during git hook notification creation\n\n${code}`
+                        )
+                    }
+
+                    currentGitHookPathResult.result = '.git/hooks'
+                }
+            })
+        }
+
+        const gitHookPath = path.join(
+            gitRootResult.result,
+            currentGitHookPathResult.result.trim(),
+            'post-checkout'
+        )
+
+        if (!(await pathExists(gitHookPath))) {
+            tasks.push({
+                title: 'Copying Git Hook Template',
+                task: async () => {
+                    await fs.promises.cp(gitHookTemplatePath, gitHookPath)
+                }
+            })
+        }
+
+        // check if gitHookPath is executable
+        const isExecutable = isExec(gitHookPath)
+        if (!isExecutable) {
+            tasks.push({
+                title: 'Making Git Hook Executable',
+                task: async () => {
+                    await execAsyncSpawn(`chmod +x ${gitHookPath}`, {
+                        withCode: true
+                    })
+                }
+            })
+        }
+
+        if (tasks.length > 0) {
+            return task.newListr(tasks)
+        }
+
+        task.skip()
+    },
+    exitOnError: false
 })
 
 module.exports = createGitHookNotification
