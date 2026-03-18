5baba207edbd6b7aa211badffae25975f128e4a8
Okabe Rintaro <ejnshtein@gmail.com>
2026-03-18 22:44:00 +0200

    Add CMA_NO_OPEN env variable to allow controlling opening of the browser not only via option in start command

--- build-packages/magento-scripts/lib/util/open-browser.js
+++ build-packages/magento-scripts/lib/util/open-browser.js
@@ -1,11 +1,18 @@
 const { execAsync } = require('./exec-async')
 
+const isCMANoOpen =
+    process.env.CMA_NO_OPEN === '1' || process.env.CMA_NO_OPEN === 'true'
+
 // eslint-disable-next-line no-control-regex
 const consoleStyleReplacer = /[\u001b]\[\S+?m/g
 /**
  * @param {string} url
  */
 const openBrowser = async (url) => {
+    if (isCMANoOpen) {
+        return
+    }
+
     const start = process.platform === 'darwin' ? 'open' : 'xdg-open'
 
     await execAsync(`${start} ${url.replace(consoleStyleReplacer, '')}`)
